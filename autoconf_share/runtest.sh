5942347be962e38a584d126fdcc10e1419e04977
Ger van Diepen <diepen@astron.nl>
2002-05-29 08:43:36 +0000

    %[BugId: 24]% Made a script findpkg (derived from lofarconf) and use it in lofarconf and runtest.sh.

--- autoconf_share/runtest.sh
+++ autoconf_share/runtest.sh
@@ -5,21 +5,31 @@ if test $# -ne 1; then
   exit 0
 fi
 
+# Get directory of this script.
+lfr_share_dir=`dirname $0`
+lfr_share_dir=`cd $lfr_share_dir && pwd`
+
+# Set source directory if not defined.
+if [ "$srcdir" = "" ]; then
+  curwd=`pwd`
+  basenm=`basename $curwd`
+  srcdir=$lfr_share_dir/../`cd .. && $lfr_share_dir/findpkg`/$basenm
+fi
+
 #
 # Copy expected files to current directory
 #
-if test -f $srcdir/$1.in; then
+if test -f "$srcdir/$1.in"; then
     \cp $srcdir/$1.in  .
 fi
-if test -f $srcdir/$1.out; then
+if test -f "$srcdir/$1.out"; then
     \cp $srcdir/$1.out .
 fi
-if test -f $srcdir/$1.run; then
+if test -f "$srcdir/$1.run"; then
     \cp $srcdir/$1.run .
 fi
 
 #
 # Run assay
 #
-lofar_dir=`dirname $0`
-$lofar_dir/assay $1
+$lfr_share_dir/assay $1
