ea0faf751bc53f4f52a6b0d79724494260e0c709
Oleg Smirnov <osmirnov@gmail.com>
2003-04-02 13:22:11 +0000

    %[BugId: 18]% Added Octopussy.cc

---
+++ OCTOPUSSY/src/Octopussy.cc
@@ -0,0 +1,80 @@
+#include <Common/Debug.h>
+#include "Dispatcher.h"
+#include "Gateways.h"
+#include "LoggerWP.h"
+
+namespace Octopussy
+{
+
+Dispatcher *pdsp = 0;
+
+Thread::ThrID thread = 0;
+  
+        
+Dispatcher &  init     ()
+{
+  FailWhen( pdsp,"OCTOPUSSY already initialized" );
+  pdsp = new Dispatcher;
+  pdsp->attach(new LoggerWP(10,Message::LOCAL),DMI::ANON);
+  initGateways(*pdsp);
+  return *pdsp;
+}
+
+void          start    ()
+{
+  FailWhen( !pdsp,"OCTOPUSSY not initialized" );
+  pdsp->start();
+  
+}
+
+void          stop     ()
+{
+  FailWhen( !pdsp,"OCTOPUSSY not initialized" );
+  pdsp->stop();
+}
+
+void          pollLoop ()
+{
+  FailWhen( !pdsp,"OCTOPUSSY not initialized" );
+  pdsp->pollLoop();
+}
+
+void          destroy  ()
+{
+  FailWhen( !pdsp,"OCTOPUSSY not initialized" );
+  delete pdsp;
+  pdsp = 0;
+}
+
+
+Thread::ThrID  initThread (bool wait_for_start)
+{
+#ifndef USE_THREADS
+  Throw("OCTOPUSSY was built w/o thread support");
+#else
+  if( !pdsp )
+    init();
+  return thread = pdsp->startThread(wait_for_start);
+#endif
+}
+
+void           stopThread  ()
+{
+#ifndef USE_THREADS
+  Throw("OCTOPUSSY was built w/o thread support");
+#else
+  stop();
+#endif
+}
+
+Thread::ThrID  threadId    ()
+{
+  return thread;
+}
+
+Dispatcher &   dispatcher  ()
+{
+  return *pdsp;
+}
+
+};
