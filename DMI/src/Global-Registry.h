9d00ea029c8813bf98a55a8ebac8e8b186ef3da3
Oleg Smirnov <osmirnov@gmail.com>
2003-04-02 13:13:15 +0000

    %[BugId: 26]% Added a global AID registry, plus makefile rules to regenerate one.

---
+++ DMI/src/Global-Registry.h
@@ -0,0 +1,9 @@
+#ifndef DMI_GLOBAL_REGISTRY_h
+#define DMI_GLOBAL_REGISTRY_h 1
+    
+// call this function to pull in the global AID registry
+// (i.e. AID registry for all packages)
+int aidRegistry_Global ();
+
+#endif
+    
