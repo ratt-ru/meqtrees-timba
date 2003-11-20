a2481dd9f4d780e3f8b755b27108e2787dfaca5e
Oleg Smirnov <osmirnov@gmail.com>
2003-11-20 10:41:39 +0000

    %[ER: 37]% Commiting working version of auto-generated classes

---
+++ MeqGen/src/meqCopy.cc
@@ -0,0 +1,33 @@
+//# meqCopy.cc: 
+//# MeqExpr node class, automatically generated from Glish
+//#
+//# Copyright  (C)  2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2,  7990 AA Dwingeloo, The Netherlands,  seg@astron.nl
+//# 
+//# $Id$: 
+
+#include "meqCopy.h"
+#include <MEQ/Vells.h>
+#include <Common/Debug.h>
+
+#define   meqFlagger        *(values[0])
+
+
+using namespace Meq::VellsMath;
+namespace meq {
+
+  Copy::Copy()
+  {}
+
+  Copy::~Copy()
+  {}
+
+  void Copy::evaluateVells (Vells& result, const Request&, const vector<Vells*>& values)
+  {
+    result = ( meqFlagger );
+  }
+
+
+} // namespace meq
+
---
+++ MeqGen/src/meqCopy.h
@@ -0,0 +1,39 @@
+//# meqCopy.h: 
+//# MeqExpr node class, automatically generated from Glish
+//#
+//# Copyright  (C)  2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2,  7990 AA Dwingeloo, The Netherlands,  seg@astron.nl
+//# 
+//# $Id$: 
+
+#ifndef MEQ_meqCopy_H
+#define MEQ_meqCopy_H
+#include <MEQ/Function.h>
+#pragma aidgroup MeqGen
+#pragma types #meq::Copy
+
+
+namespace meq {
+
+  using namespace Meq;
+
+  class Copy : public Function
+  {
+
+    public:
+
+      Copy();
+
+      virtual ~Copy();
+
+      virtual void evaluateVells (Vells& result, const Request&, const vector<Vells*>& values);
+
+
+    private:
+
+  };
+
+} // namespace meq
+
+#endif
