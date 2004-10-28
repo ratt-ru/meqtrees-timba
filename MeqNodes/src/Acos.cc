5ab78fd2364bb63507ce5c020f628c3bc16f13d1
assendorp <info@astron.nl>
2004-10-28 06:02:56 +0000

    %[ER: ]% first code

---
+++ MeqNodes/src/Acos.cc
@@ -0,0 +1,45 @@
+//# Acos.cc: Take arc-cosine of a node
+//#
+//# Copyright (C) 2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+
+#include <MeqNodes/Acos.h>
+
+#include <MEQ/Vells.h>
+
+using namespace Meq::VellsMath;
+
+namespace Meq {    
+
+//##ModelId=400E53550241
+Acos::Acos()
+{}
+
+//##ModelId=400E53550242
+Acos::~Acos()
+{}
+
+//##ModelId=400E53550246
+Vells Acos::evaluate (const Request&,const LoShape &,
+		     const vector<const Vells*>& values)
+{
+  return acos(*(values[0]));
+}
+
+} // namespace Meq
