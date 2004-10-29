335236bf71d3fdf5d7623eadb67e78bf563d9db2
Tony Willis <twillis@drao.nrc.ca>
2004-10-29 20:08:21 +0000

    %[ER: ]% Modified the data display code a bit so it is closer to that in result_plotter.py for possible later combination

--- PyApps/src/array_plotter.py
+++ PyApps/src/array_plotter.py
@@ -43,16 +43,23 @@ class ArrayPlotter(BrowserPlugin):
 
   def display_data (self, plot_array):
 # figure out type and rank of incoming array
+    is_vector = False;
     array_dim = len(plot_array.shape)
     array_rank = plot_array.rank
+    if array_rank == 1:
+      is_vector = True;
     n_rows = plot_array.shape[0]
+    if n_rows == 1:
+      is_vector = True
     n_cols = 1
     if array_rank == 2:
       n_cols = plot_array.shape[1]
+      if n_cols == 1:
+        is_vector = True
     self._add_x_y = False;
 
 # test if we have a 2-D array
-    if n_cols > 1:
+    if is_vector == False:
 # first display an image 
       if self._image_ntuple == None:
         self._image_ntuple = self._ntuple_controller.createNTuple()
@@ -68,7 +75,6 @@ class ArrayPlotter(BrowserPlugin):
       else:
 # add columns for new image data
         self._image_ntuple.addColumn (self._label,image)
-#        print "added column"
 # add time and frequency columns for xyz plots
 # first add x column
         if self._add_x_y:
@@ -78,7 +84,6 @@ class ArrayPlotter(BrowserPlugin):
             for i in range(0, n_cols) :
               image.append(j)
           self._image_ntuple.addColumn (xyz_x_label, image)
-#          print "added x column"
 # then add y column
           xyz_y_label = "y"
           image = []
@@ -86,22 +91,17 @@ class ArrayPlotter(BrowserPlugin):
             for i in range(0, n_cols) :
               image.append(i)
           self._image_ntuple.addColumn (xyz_y_label, image)
-#          print "added y column"
           self._add_x_y = False
 
 # do image plot
         image_plot = self._display_controller.createDisplay( 'Z Plot', self._image_ntuple,[self._label,])
-#        print "created image_plot object"
         image_plot.setLabel ( 'x', 'x' )
         image_plot.setLabel ( 'y', 'y' )
         image_plot.setNumberOfBins ( 'x', n_rows )
         image_plot.setNumberOfBins ( 'y', n_cols )
-#        print "passed image_plot.setNumberOfBins"
         if self._canvas == None:
           self._canvas = self._window_controller.currentCanvas()
-#          print "created self._canvas object"
         self._canvas.addDisplay ( image_plot ) 
-#        print "called self._canvas.addDisplay(image_plot)"
 
 # now do an XYZ plot
         bindings = ["x", "y",  self._label ]
@@ -113,16 +113,23 @@ class ArrayPlotter(BrowserPlugin):
         self._canvas.addDisplay ( xyz_plot )
 #        print "called self._canvas.addDisplay(xyz_plot)"
                                                                                 
-    if n_cols == 1:
+    if is_vector == True:
       if self._simple_ntuple == None:
 # do X-Y plot
         self._simple_ntuple = self._ntuple_controller.createNTuple()
         self._simple_ntuple.setTitle ( "Array Data" )
         self._add_x = True
       image = []
-      for j in range(0, n_rows) :
+      if n_rows > 1:
+        for j in range(0, n_rows) :
          if array_rank == 2:
            image.append(plot_array[j][0])
+         else:
+           image.append(plot_array[j])
+      if n_cols > 1:
+        for j in range(0, n_cols) :
+         if array_rank == 2:
+           image.append(plot_array[0][j])
          else:
            image.append(plot_array[j])
       if self._simple_ntuple.isValidLabel(self._label):
@@ -132,11 +139,14 @@ class ArrayPlotter(BrowserPlugin):
         self._simple_ntuple.addColumn (self._label,image)
 # first add x column
         if self._add_x:
-          xyz_x_label = "x"
           image = []
-          for j in range(0, n_rows ) :
-            image.append(j)
-          self._simple_ntuple.addColumn (xyz_x_label, image)
+          if n_cols > 1:
+            for j in range(0, n_cols ) :
+              image.append(j)
+          if n_rows > 1:
+            for j in range(0, n_rows ) :
+              image.append(j)
+          self._simple_ntuple.addColumn ("x", image)
           self._add_x = False
         plot = self._display_controller.createDisplay ( 'XY Plot', self._simple_ntuple, ["x",self._label] )
         if self._canvas == None:
@@ -153,14 +163,14 @@ class ArrayPlotter(BrowserPlugin):
     if complex_type:
 #extract real component
       real_array = dataitem.data.getreal()
-      self._label = "real"
+      self._label = "real data value"
       self.display_data(real_array)
 #extract imaginary component
       imag_array = dataitem.data.getimag()
-      self._label = "imag"
+      self._label = "imag data value"
       self.display_data(imag_array)
     else:
-      self._label = "real"
+      self._label = "real data value"
       self.display_data(dataitem.data)
     
 gridded_workspace.registerViewer(array_class,ArrayPlotter,priority=-10)
