0d078a3a38f2401b1796aeec835685e1a274a90d
Tony Willis <twillis@drao.nrc.ca>
2005-02-17 09:30:32 +0000

    %[ER: ]% Now uses modulus to determine divisions

--- PyApps/src/ComplexScaleDraw.py
+++ PyApps/src/ComplexScaleDraw.py
@@ -12,7 +12,8 @@ class ComplexScaleDraw (QwtScaleDraw):
         self.divisor = divisor
 
     def label(self,v):
-        if v > self.divisor:
-          v = v - self.divisor
+#       if v > self.divisor:
+#         v = v - self.divisor
+	v = v % self.divisor
         return QwtScaleDraw.label(self,v)
 
