7104fe770a2418744ee262626190e0b143253095
Oleg Smirnov <osmirnov@gmail.com>
2005-02-23 18:20:07 +0000

    %[ER: ]% Added rereadDir() call to save/load forest, to keep file list in dialog up-to-date.

--- PyApps/src/treebrowser.py
+++ PyApps/src/treebrowser.py
@@ -735,6 +735,8 @@ Please press OK to confirm.""",QMessageBox.Ok,\
       dialog.setFilters("Forests (*.forest *.meqforest);;All files (*.*)");
       dialog.setViewMode(QFileDialog.Detail);
       dialog.setCaption("Save forest");
+    else:
+      dialog.rereadDir();
     if dialog.exec_loop() == QDialog.Accepted:
       fname = str(dialog.selectedFile());
       rec = srecord(file_name=fname,get_forest_status=True);
@@ -748,6 +750,8 @@ Please press OK to confirm.""",QMessageBox.Ok,\
       dialog.setFilters("Forests (*.forest *.meqforest);;All files (*.*)");
       dialog.setViewMode(QFileDialog.Detail);
       dialog.setCaption("Load forest");
+    else:
+      dialog.rereadDir();
     if dialog.exec_loop() == QDialog.Accepted:
       fname = str(dialog.selectedFile());
       rec = srecord(file_name=fname,get_forest_status=True);
@@ -953,4 +957,4 @@ class NA_ContinueUntil (NodeAction):
 
 ### import plug-ins
 import node_execute
-
+import RBtest
