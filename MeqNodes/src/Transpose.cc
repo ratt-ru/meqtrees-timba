849df0de7d88d30f6375b01e6213aedaf22e1e81
Oleg Smirnov <osmirnov@gmail.com>
2005-04-11 13:35:59 +0000

    %[ER: ]% Added transpose node

---
+++ MeqNodes/src/Transpose.cc
@@ -0,0 +1,110 @@
+//# Transpose.cc: Selects result planes from a result set
+//#
+//# Copyright (C) 2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#include <MeqNodes/Transpose.h>
+#include <MeqNodes/AID-MeqNodes.h>
+#include <MEQ/Request.h>
+#include <MEQ/VellSet.h>
+#include <MEQ/Cells.h>
+#include <MEQ/MeqVocabulary.h>
+
+namespace Meq {    
+
+const HIID FConj = AidConj;
+
+//##ModelId=400E5355022C
+Transpose::Transpose()
+    : Node(1),conj_(false) // exactly 1 child expected
+{}
+
+//##ModelId=400E5355022D
+Transpose::~Transpose()
+{}
+
+//##ModelId=400E53550233
+void Transpose::setStateImpl (DMI::Record::Ref &rec,bool initializing)
+{
+  Node::setStateImpl(rec,initializing);
+  rec[FConj].get(conj_,initializing);
+}
+
+void Transpose::conjugate (VellSet &vs)
+{
+  using VellsMath::conj;
+  
+  if( !vs.hasValue() )
+    return;
+  vs.setValue(new Vells(conj(vs.getValue())));
+  
+  if( vs.numSpids() )
+  {
+    for( int i=0; i<vs.numPertSets(); i++ )
+      for( int j=0; j<vs.numSpids(); j++ )
+        vs.setPerturbedValue(j,new Vells(conj(vs.getPerturbedValue(j,i))),i);
+  }
+}
+
+//##ModelId=400E53550237
+int Transpose::getResult (Result::Ref &resref, 
+                         const std::vector<Result::Ref> &childref,
+                         const Request &request,bool)
+{
+  resref = childref[0];
+  int rank = resref->tensorRank();
+  FailWhen(rank>2,"illegal tensor rank in transpose");
+  int nvs = resref->numVellSets();
+  if( rank == 1 )
+  {
+    LoShape dims(1,resref->numVellSets());
+    resref().setDims(dims);
+  }
+  else if( rank == 2 )
+  {
+    const Result &chres = *childref[0];
+    int nx = chres.dims()[0], 
+        ny = chres.dims()[1];
+    if( nx == 1 ) // a 1xN element -- just reshape
+      resref().setDims(LoShape(ny));
+    else
+    {
+      Result &result = resref();
+      result.setDims(LoShape(ny,nx));
+      int offs = 0;
+      for( int i=0; i<ny; i++ )
+        for( int j=0; j<nx; j++,offs++ )
+          result.setVellSet(offs,chres.vellSet(j*ny+i));
+    }
+  }
+  // NOP for scalar (rank=0)
+  
+  // conjugate as needed
+  if( conj_ )
+  {
+    Result &result = resref();
+    for( int i=0; i<nvs; i++ )
+      conjugate(result.vellSetWr(i));
+  }
+  
+  return 0;
+}
+
+} // namespace Meq
---
+++ MeqNodes/src/Transpose.h
@@ -0,0 +1,79 @@
+//# Transpose.h: Selects result planes from a result set
+//#
+//# Copyright (C) 2003
+//# ASTRON (Netherlands Foundation for Research in Astronomy)
+//# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful,
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//# GNU General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//#
+//# $Id$
+
+#ifndef MEQNODES_TRANSPOSE_H
+#define MEQNODES_TRANSPOSE_H
+    
+#include <MEQ/Node.h>
+
+#include <MeqNodes/TID-MeqNodes.h>
+#pragma aidgroup MeqNodes
+
+#pragma types #Meq::Transpose
+
+#pragma aid Conj
+
+// The comments below are used to automatically generate a default
+// init-record for the class 
+
+//defrec begin MeqTranspose
+//  A MeqTranspose tranposes a matrix. Only one child is expected. The child
+//  must return a tensor rank <=2 result. Matrices are transposed, vectors
+//  are turned into a 1xN tensor, and scalars pass through unchanged.
+//field: conj F
+//  If true, does a complex transpose-and-conjugate operation.
+//defrec end
+
+namespace Meq {    
+
+//##ModelId=400E53040077
+class Transpose : public Node
+{
+  public:
+    //##ModelId=400E5355022C
+    Transpose ();
+    //##ModelId=400E5355022D
+    virtual ~Transpose ();
+    
+    //##ModelId=400E5355022F
+    virtual TypeId objectType() const
+    { return TpMeqTranspose; }
+    
+  protected:
+    //##ModelId=400E53550233
+    virtual void setStateImpl (DMI::Record::Ref &rec,bool initializing);
+    //##ModelId=400E53550237
+    virtual int getResult (Result::Ref &resref, 
+                           const std::vector<Result::Ref> &childres,
+                           const Request &req,bool newreq);
+  
+  private:
+    void conjugate (VellSet &vs);
+      
+    //##ModelId=400E5355022A
+    bool conj_;
+};
+
+
+} // namespace Meq
+
+#endif
