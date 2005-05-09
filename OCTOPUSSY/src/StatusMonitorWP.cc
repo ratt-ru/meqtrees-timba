310a382fae8d3826c2c696a68a09c4f5bfe31385
Oleg Smirnov <osmirnov@gmail.com>
2005-05-09 07:47:41 +0000

    %[ER: ]%

---
+++ OCTOPUSSY/src/StatusMonitorWP.cc
@@ -0,0 +1,118 @@
+#include "StatusMonitorWP.h"
+
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
+
+namespace Octopussy
+{
+
+StatusMonitorWP::StatusMonitorWP (int ms,AtomicID wpid)
+ : WorkProcess(wpid),
+   period_ms_(ms),
+   prefix_("Process.Status")
+{
+  using Debug::ssprintf;
+  pagesize_ = sysconf(_SC_PAGESIZE)>>10;
+  string statfilename = ssprintf("/proc/%d/statm",(int)getpid());
+  fstat_ = fopen(statfilename.c_str(),"r");
+  cerr<<"stat file: "<<fstat_<<endl;
+}
+
+StatusMonitorWP::~StatusMonitorWP()
+{
+  if( fstat_ )
+    fclose(fstat_);
+}
+
+//##ModelId=3C7E4AC70261
+bool StatusMonitorWP::start ()
+{
+  WorkProcess::start();
+  if( period_ms_ )
+    addTimeout(period_ms_*1e-3,AidBroadcast,EV_CONT);
+  return false;
+}
+
+void StatusMonitorWP::setPeriod (int ms)
+{
+  if( isRunning() )
+  {
+    if( period_ms_ )
+      removeTimeout(AidBroadcast);
+    addTimeout((period_ms_=ms)*1e-3,AidBroadcast,EV_CONT);
+  }
+  else
+    period_ms_ = ms;
+}
+
+void StatusMonitorWP::makeStatusMessage (Message::Ref &msg)
+{
+//   // get limits
+//   struct rlimit rlim_data;
+//   struct rlimit rlim_rss;
+//   getrlimit(RLIMIT_DATA,&rlim_data);
+//   getrlimit(RLIMIT_RSS,&rlim_rss);
+  // get memory status
+  int tot_pg=0,mem_pg=0,shared_pg=0,code_pg=0,data_pg=0,lib_pg=0,dirty_pg=0;
+  if( fstat_ )
+  {
+    rewind(fstat_);
+    fscanf(fstat_,"%d %d %d %d %d %d %d",&tot_pg,&mem_pg,&shared_pg,&code_pg,&data_pg,&lib_pg,&dirty_pg);
+    cerr<<"mem stats: "<<tot_pg<<" "<<mem_pg<<" "<<shared_pg<<" "<<code_pg<<" "<<data_pg<<" "<<lib_pg<<" "<<dirty_pg<<endl;
+  }
+  // get cpu time usage
+  struct rusage ru;
+  getrusage(RUSAGE_SELF,&ru);
+  // form message
+  HIID msgid = prefix_;
+  int i = prefix_.size();
+  msgid.resize(i+9);
+  msgid[i++] = tot_pg*pagesize_;
+  msgid[i++] = mem_pg*pagesize_;
+  msgid[i++] = shared_pg*pagesize_;
+  msgid[i++] = code_pg*pagesize_;
+  msgid[i++] = data_pg*pagesize_;
+  msgid[i++] = lib_pg*pagesize_;
+  msgid[i++] = dirty_pg*pagesize_;
+  msgid[i++] = ru.ru_utime.tv_sec;
+  msgid[i++] = ru.ru_utime.tv_usec;
+  // create message 
+  msg <<= new Message(msgid);
+}
+
+int StatusMonitorWP::timeout (const HIID &)
+{
+  Message::Ref msg;
+  makeStatusMessage(msg);
+  // send it off
+  publish(msg);
+  return Message::ACCEPT;
+}
+
+//##ModelId=3C7E49AC014C
+int StatusMonitorWP::receive (Message::Ref& mref)
+{
+  // print the message starting at debug level 2
+  if( Debug(2) )
+  {
+    int level = getDebugContext().level() - 1;
+    dprintf(2)("received: %s\n",mref->sdebug(level).c_str());
+  }
+  // process message
+  const HIID &id = mref->id();
+  if( id.matches("Set.Period.?") )
+  {
+    setPeriod(id[2]);
+  }
+  else if( id.matches("Request") )
+  {
+    Message::Ref response;
+    makeStatusMessage(response);
+    send(response,mref->from());
+  }
+  return Message::ACCEPT;
+}
+
+};
---
+++ OCTOPUSSY/src/StatusMonitorWP.h
@@ -0,0 +1,53 @@
+#ifndef _OCTOPUSSY_StatusMonitorWP_h
+#define _OCTOPUSSY_StatusMonitorWP_h 1
+
+#include <OCTOPUSSY/WorkProcess.h>
+#include <OCTOPUSSY/AID-OCTOPUSSY.h>
+#include <stdio.h>
+        
+#pragma aid StatusMonitorWP Period Process Status Broadcast Request Set
+
+namespace Octopussy
+{
+using namespace DMI;
+
+
+class StatusMonitorWP : public WorkProcess
+{
+  public:
+      StatusMonitorWP (int period_ms=1000,AtomicID wpid = AidStatusMonitorWP);
+
+      ~StatusMonitorWP();
+
+      virtual bool start ();
+      
+      virtual int timeout (const HIID &);
+
+      virtual int receive (Message::Ref& mref);
+      
+      void setPeriod (int ms);
+      
+      void setPrefix (const HIID &prefix)
+      { prefix_ = prefix; }
+
+  private:
+      void makeStatusMessage (Message::Ref &msg);
+      
+      int period_ms_;
+      
+      HIID prefix_;
+      
+      int pagesize_;
+      
+      FILE *fstat_;
+      
+      StatusMonitorWP(const StatusMonitorWP &right);
+
+      StatusMonitorWP & operator=(const StatusMonitorWP &right);
+};
+
+// Class StatusMonitorWP 
+
+
+};
+#endif
