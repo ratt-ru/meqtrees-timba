0049ba7bba0435859dc2ec1f1a585981cfe20a1f
Tony Willis <twillis@drao.nrc.ca>
2005-05-12 21:13:12 +0000

    %[ER: ]% Added ComplexScaleSeparate function to handle x-axis coordinates properly when using Frequency units

--- PyApps/src/Plugins/ComplexScaleDraw.py
+++ PyApps/src/Plugins/ComplexScaleDraw.py
@@ -15,3 +15,17 @@ class ComplexScaleDraw (QwtScaleDraw):
 	v = v % self.divisor
         return QwtScaleDraw.label(self,v)
 
+class ComplexScaleSeparate (QwtScaleDraw):
+
+    def __init__(self,start_freq, end_freq):
+        QwtScaleDraw.__init__(self)
+        self.start_freq = start_freq
+        self.end_freq = end_freq
+        self.delta = self.end_freq - self.start_freq
+    # __init__()
+
+    def label(self,v):
+        if v > self.end_freq:
+          v = v - self.delta
+        return QwtScaleDraw.label(self,v)
+
