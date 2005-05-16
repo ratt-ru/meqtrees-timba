752634a7ba23818a4be648674ece5f91722b4abb
Oleg Smirnov <osmirnov@gmail.com>
2005-05-16 13:20:36 +0000

    %[ER: ]% Added choice of allocators to Allocators.h

--- DMI/src/Allocators.h
+++ DMI/src/Allocators.h
@@ -2,22 +2,30 @@
 #define DMI_Allocators_h 1
 
 // with non-gnu compilers, fall back to standard allocator
-#if __GNUC__ < 3    
-  #undef  DMI_USE_STD_ALLOC
+#if __GNUC__ < 3
   #define DMI_USE_STD_ALLOC 1
 #endif
             
 // DMI_USE_STD_ALLOC: define this to use the standard allocator (new/delete)
 // rather than the pool-based multithreaded one
 
-#if DMI_USE_STD_ALLOC
+#if defined(DMI_USE_STD_ALLOC)
 
   #define DMI_Allocator std::alloc
+  
+#elsif defined(DMI_USE_POOL_ALLOC)
 
-#else
+  #include <ext/pool_allocator.h>
+  #define DMI_Allocator __gnu_cxx::__pool_alloc
+
+#elsif defined(DMI_USE_BITMAP_ALLOC)
+
+  #include <ext/bitmap_allocator.h>
+  #define DMI_Allocator __gnu_cxx::bitmap_allocator
+  
+#else // default is mt_alloc
 
   #include <ext/mt_allocator.h>
-
   #define DMI_Allocator __gnu_cxx::__mt_alloc
   
 #endif
