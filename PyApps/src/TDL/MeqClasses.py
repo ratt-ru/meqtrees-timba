fbb646bc4afc98316c510ff63a4c68acd56ff7f9
Oleg Smirnov <osmirnov@gmail.com>
2005-06-21 13:11:54 +0000

    Streamlined TDL classes.

--- PyApps/src/TDL/MeqClasses.py
+++ PyApps/src/TDL/MeqClasses.py
@@ -8,13 +8,13 @@ _dbg = TDLimpl._dbg;
 _dprint = _dbg.dprint;
 _dprintf = _dbg.dprintf;
 
-class _MeqNS (TDLimpl.ClassNamespace):
-  """This class defines the standard 'Meq' namespace. Having a specialized
-  _MeqNS class defining 'Meq' (as opposed to using the generic 
-  ClassNamespace class) allows us to implement specific constructors for 
+class _MeqGen (TDLimpl.ClassGen):
+  """This class defines the standard 'Meq' class generator. Having a 
+  specialized generator for the Meq classes (as opposed to using a generic 
+  ClassGen object) allows us to implement specific constructors for 
   complicated nodes such as Meq.Parm and Meq.Solver.""";
   def __init__ (self):
-    TDLimpl.ClassNamespace.__init__(self,'Meq');
+    TDLimpl.ClassGen.__init__(self,'Meq');
     
   def Parm (self,funklet=None,**kw):
     if funklet is not None:
@@ -27,7 +27,7 @@ class _MeqNS (TDLimpl.ClassNamespace):
           if _dbg.verbose>0:
             traceback.print_exc();
           raise TDLimpl.NodeDefError,"illegal funklet argument in call to Meq.Parm at "+TDLimpl._identifyCaller();
-    return TDLimpl.ClassNamespace._make_nodespec('MeqParm',None,kw);
+    return TDLimpl._NodeDef('MeqParm',**kw);
     
   def Solver (self,*childlist,**kw):
     solvables = kw.get('solvable',None);
@@ -39,21 +39,17 @@ class _MeqNS (TDLimpl.ClassNamespace):
       kw['solvable'] = dmi.record(
         command_by_list=[dmi.record(name=solvables,state=dmi.record(solvable=True)),
                          dmi.record(state=dmi.record(solvable=False))]);
-    return TDLimpl.ClassNamespace._make_nodespec('MeqSolver',childlist,kw);
+    return TDLimpl._NodeDef('MeqSolver',*childlist,**kw);
     
   # now for some aliases
-  def Matrix22 (self,a,b,c,d):
+  def Matrix22 (self,a,b,c,d,**kw):
     "composes a 2x2 matrix as [[a,b],[c,d]]";
-    return self.Composer(a,b,c,d,dims=[2,2]);
+    kw['dims'] = [2,2];
+    return self.Composer(a,b,c,d,**kw);
     
-  def ConjTranspose (self,arg):
+  def ConjTranspose (self,arg,**kw):
     "conjugate (hermitian) transpose";
-    return self.Transpose(arg,conj=True);
+    kw['conj'] = True;
+    return self.Transpose(arg,**kw);
     
-Meq = _MeqNS();
-  
-  
-
-
-
-
+Meq = _MeqGen();
--- PyApps/src/TDL/TDLimpl.py
+++ PyApps/src/TDL/TDLimpl.py
@@ -30,26 +30,58 @@ class NodeDefError (TDLError):
   """this error is raised when a node is incorrectly defined""";
   pass;
   
-class _NodeSpec (object):
-  """this represents a node specification, as returned by a node class call""";
+
+class _NodeDef (object):
+  """this represents a node defintion, as returned by a node class call""";
   __slots__ = ("children","initrec");
-  def __init__ (self,children,initrec):
-    (self.children,self.initrec) = (children,initrec);
+  def __init__ (self,classname,*childlist,**kw):
+    """Creates a _NodeDef object for a node of the given classname.
+    Children are built up from either the argument list, or the 'children'
+    keyword (if both are supplied, an error is thrown), or from keywords of
+    type '_NodeDef' or '_NodeStub', or set to None if no children are specified. 
+    The initrec is built from the remaining keyword arguments, with the field
+    class=classname inserted as well.
+    """;
+    # figure out children
+    children = kw.pop('children',None);
+    if children:
+      if childlist: raise ChildError,"children specified both by arguments and keyword at "+_identifyCaller();
+    else:  # no children dict, use list if we got it
+      if childlist: children = childlist;
+      else: # else see if some keywords specify children
+        children = {};
+        for (key,val) in kw.iteritems():
+          if isinstance(val,(_NodeDef,_NodeStub)):
+            children[key] = val;
+        # remove from keyword set if found any   
+        if children:
+          map(kw.pop,children.iterkeys());
+        else:
+          children = None;
+    # create init-record 
+    initrec = dmi.record(**kw);
+    initrec['class'] = classname;
+    # ensure type of node_groups argument (so that strings are implicitly converted to hiids)
+    groups = getattr(initrec,'node_groups',None);
+    if groups is not None:
+      initrec.node_groups = dmi.make_hiid_list(groups);
+    self.children = children;
+    self.initrec = initrec;
   def resolve (arg,recurse=5):
-    """static method to resolve an argument to a _NodeSpec object, or return None on error.
+    """static method to resolve an argument to a _NodeDef object, or return None on error.
     This method implements some implicit ways to create a node:
       * by specifying a constant value
       * by specifying a node class (such as Meq.Parm) without arguments
       * by specifying anything callable that returns one of the above
     """;
-    if isinstance(arg,_NodeSpec):
+    if isinstance(arg,_NodeDef):
       return arg;
-    if isinstance(arg,(complex,float)):
+    elif isinstance(arg,complex):
       return _Meq.Constant(value=arg);
     if isinstance(arg,(bool,int,long,float)):
       return _Meq.Constant(value=float(arg));
     if callable(arg) and recurse>0:
-      return _NodeSpec.resolve(arg(),recurse=recurse-1);
+      return _NodeDef.resolve(arg(),recurse=recurse-1);
     return None;
   resolve = staticmethod(resolve);
   # define implicit arithmetic
@@ -70,29 +102,20 @@ class _NodeSpec (object):
   def __rdiv__ (self,other):
     return _Meq.Divide(other,self);
   
-def _identifyCaller (depth=3,skip_internals=True):
-  """Identifies source location from which function was called.
-  Normal depth is 3, corresponding to the caller of the caller of
-  _identifyCaller(), but if skip_internals=True, this will also 
-  skip over calls to built-ins.""";
-  stack = traceback.extract_stack();
-  if skip_internals:
-    for frame in stack[-depth::-1]:
-      (filename,line,funcname,text) = frame;
-      if not filename.endswith('TDLimpl.py'):
-        break;
-    # got to the end of the stack without finding a non-built-in? Fall back 
-    # to starting frame
-    if funcname.startswith('__'):
-      (filename,line,funcname,text) = stack[-depth];
-  else:
-    (filename,line,funcname,text) = stack[-depth];
-  filename = filename.split('/')[-1];
-  return "%s:%d:%s" % (filename,line,funcname);
-  
-class _NodeRepr (object):
-  def __init__ (self,name,scope,basename,quals,kwquals):
-    self.name = name;
+class _NodeStub (object):
+  """A NodeStub represents a node. Initially a stub is created with only
+  a name. To make a fully-fledged node, a stub must be bound with a NodeDef 
+  object via the bind() method, or the << operator. One bound, the 
+  node stub is added to its repository.
+  A stub may also be qualified via the () operator, this creates new node stubs
+  with qualified names.
+  """;
+  slots = ( "name","scope","basename","quals","kwquals",
+            "classname","parents","children",
+            "_initrec","_debuginfo" );
+  def __init__ (self,fqname,basename,scope,*quals,**kwquals):
+    _dprint(5,'creating node stub',fqname,basename,scope._name,quals,kwquals);
+    self.name = fqname;
     self.scope = scope;
     self.basename = basename;
     self.quals = quals;
@@ -101,17 +124,28 @@ class _NodeRepr (object):
     self.parents = weakref.WeakValueDictionary();
     self._initrec = None;         # uninitialized node
     # figure out source location from where node was defined.
-    self._debuginfo = "%s(%s)" % (name,_identifyCaller());
+    self._debuginfo = "%s(%s)" % (basename,_identifyCaller());
+  def bind (self,arg,*args,**kwargs):
+    """The bind() method is an alternative form of <<. If called with
+    a str as the first argument, it is assumed to be a classname, and a 
+    NodeDef is constructed using all the arguments. This NodeDef is bound 
+    with the normal << call.
+    Otherwise, a straight call to << with the first argument is done.
+    """
+    if isinstance(arg,str):
+      arg = _NodeDef(arg,*args,**kwargs);
+    return self << arg;
   def __lshift__ (self,arg):
     # resolve argument to a node spec. This will throw an exception on error
-    arg = _NodeSpec.resolve(arg);
+    arg = _NodeDef.resolve(arg);
+    _dprint(4,self.name,'<<',arg);
     if arg is None:
-      raise TypeError,self._debuginfo+": cannot << something of type "+type(arg).__name__;
+      raise NodeDefError,self._debuginfo+": cannot bind node to something of type "+type(arg).__name__;
     (children,initrec) = (arg.children,arg.initrec);
     if not self.initialized():
       try: self.classname = getattr(initrec,'class');
       except AttributeError: 
-        raise ValueError,self._debuginfo+": init record missing class field";
+        raise NodeDefError,self._debuginfo+": init record missing class field";
       self._initrec = initrec;
       # normalize child list
       if children is None:
@@ -128,7 +162,7 @@ class _NodeRepr (object):
       # resolve children, check their types and mark parents
       for (i,(ich,child)) in enumerate(self.children):
         _dprint(5,'checking child',i,ich,'=',child);
-        if not isinstance(child,_NodeRepr):
+        if not isinstance(child,_NodeStub):
           if isinstance(child,str):        # child referenced by name? 
             try: child = self.scope.Repository()[child];
             except KeyError:
@@ -138,8 +172,8 @@ class _NodeRepr (object):
           elif isinstance(child,(bool,int,long,float)):
             child = self.scope.MakeConstant(float(child));
           else:
-            # try to resolve child to a _NodeSpec
-            anonch = _NodeSpec.resolve(child);
+            # try to resolve child to a _NodeDef
+            anonch = _NodeDef.resolve(child);
             if anonch is None:
               raise ChildError,"%s: child %s has illegal type %s" % (self._debuginfo,str(ich),type(child).__name__);
             _dprint(4,self.name,': creating anon child',ich);
@@ -148,6 +182,9 @@ class _NodeRepr (object):
           self.children[i] = (ich,child);
         # add ourselves to parent list
         child.parents[self.name] = self;
+      # add ourselves to repository
+      _dprint(5,'adding',self.name,'to repository with initrec',self._initrec);
+      self.scope._repository[self.name] = self;
     else: # already initialized, check for conflicts
       if self._initrec != initrec:
         _dprint(1,'old definition',self._initrec);
@@ -164,16 +201,15 @@ class _NodeRepr (object):
   def initrec (self):
     return self._initrec;
   def __call__ (self,*quals,**kwquals):
-    """Creates a node based on this one, with additional qualifiers. Returns a _NodeRepr.""";
+    """Creates a node based on this one, with additional qualifiers. Returns a _NodeStub.""";
+    quals = self.quals + quals;
     kw = self.kwquals;
     kw.update(kwquals);
-    return self.scope[self.basename](*(self.quals + quals),**kw);
-  def buddy (self,name,*quals,**kwquals):
-    """Derives a node with a different basename and the same qualifier, plus (optionally) additional qualifiers. 
-    Returns a _NodeRepr.""";
-    kw = self.kwquals;
-    kw.update(kwquals);
-    return self.scope[name](*(self.quals + quals),**kw);
+    fqname = qualifyName(self.basename,*quals,**kw);
+    try: 
+      return self.scope._repository[fqname];
+    except KeyError:
+      return _NodeStub(fqname,self.basename,self.scope,*quals,**kw);
   # define implicit arithmetic
   def __add__ (self,other):
     return _Meq.Add(self,other);
@@ -191,70 +227,38 @@ class _NodeRepr (object):
     return _Meq.Multiply(other,self);
   def __rdiv__ (self,other):
     return _Meq.Divide(other,self);
-    
-class ClassNamespace (object):
-  class _ClassRepr (object):
-    """_ClassRepr represents a node class. When called with (), it returns
+
+class ClassGen (object):
+  class _ClassStub (object):
+    """_ClassStub represents a node class. When called with (), it returns
     a tuple of (children,initrec) composed of its arguments."""
+    __slots__ = ("_name",);
     def __init__ (self,name):
       self._name = name;
-    def __call__ (self,*childlist,**kw):
-      """Calling a node class creates a _NodeSpec object. See _make_nodespec()
+    def __call__ (self,*arg,**kw):
+      """Calling a node class creates a _NodeDef object. See _make_nodespec()
       method below for details.""";
-      return ClassNamespace._make_nodespec(self._name,childlist,kw);
+      return _NodeDef(self._name,*arg,**kw);
   def __init__ (self,prefix=''):
     self._prefix = prefix;
   def __getattr__ (self,name):
-    """Accessing as, e.g., Meq.Nodeclass automatically inserts a _ClassRepr
+    """Accessing as, e.g., Meq.Nodeclass automatically inserts a _ClassStub
     attribute for 'NodeClass'.""";
     try: return object.__getattr__(self,name);
     except AttributeError: pass;
-    classrepr = self._ClassRepr(self._prefix+name);
-    setattr(self,name,classrepr);
-    return classrepr;
-  def _make_nodespec (classname,childlist,kw):
-    """helper method to make a _Nodespec object, used by _ClassRepr above.
-    Children are built up from either the argument list, or the 'children'
-    keyword (if both are supplied, an error is thrown), or from keywords of
-    type '_NodeSpec' or '_NodeRepr', or set to None if no children are specified. 
-    The initrec is built from the remaining keyword arguments, with the field
-    class=classname inserted as well.
-    """;
-    # figure out children
-    children = kw.pop('children',None);
-    if children:
-      if childlist: raise ChildError,"children specified both by arguments and keyword at "+_identifyCaller();
-    else:  # no children dict, use list if we got it
-      if childlist: children = childlist;
-      else: # else see if some keywords specify children
-        children = {};
-        for (key,val) in kw.iteritems():
-          if isinstance(val,(_NodeSpec,_NodeRepr)):
-            children[key] = val;
-        # remove from keyword set if found any   
-        if children:
-          map(kw.pop,children.iterkeys());
-        else:
-          children = None;
-    # create init-record 
-    initrec = dmi.record(**kw);
-    initrec['class'] = classname;
-    # ensure type of node_groups argument (so that strings are implicitly converted to hiids)
-    groups = getattr(initrec,'node_groups',None);
-    if groups is not None:
-      initrec.node_groups = dmi.make_hiid_list(groups);
-    return _NodeSpec(children,initrec);
-  _make_nodespec = staticmethod(_make_nodespec);
+    stubname = self._prefix+name;
+    _dprint(5,'creating class stub',stubname);
+    stub = self._ClassStub(stubname);
+    setattr(self,name,stub);
+    return stub;
   
-_Meq = ClassNamespace('Meq');
-    
 class NodeGroup (dict):
   """This represents a group of nodes, such as, e.g., root nodes. The
   << operator is redefined to add nodes to the group.""";
   def __init__ (self,name=''):
     self.name = name;
   def __lshift__ (self,node):
-    if not isinstance(node,_NodeRepr):
+    if not isinstance(node,_NodeStub):
       raise TypeError,"you may only add nodes to a node group with <<";
     dict.__setitem__(self,node.name,node);
     return node;
@@ -264,15 +268,6 @@ class NodeGroup (dict):
     try: return dict.__contains__(self,node.name);
     except AttributeError: return False;
 
-def _printNode (node,name='',offset=0):
-  header = (' ' * offset);
-  if name:
-    header += name+": ";
-  header += str(node);
-  print "%s: %.*s" % (header,78-len(header),str(node._initrec));
-  for (ich,child) in node.children:
-    _printNode(child,str(ich),offset+2);
-    
 class _NodeRepository (dict):
   def deleteOrphan (self,name):
     """recursively deletes orphaned branches""";
@@ -353,35 +348,7 @@ class _NodeRepository (dict):
     _dprint(2,"root nodes:",self._roots.keys());
     _dprint(1,len(self),"total nodes in repository");
 
-def qualifyName (name,*args,**kws):
-  """Qualifies name by appending a dict of qualifiers to it, in the form
-  of name:a1:a2:k1=v1:k2=v2, etc."""
-  qqs = list(kws.iteritems());
-  qqs.sort();
-  qqs = map(lambda x: '='.join(map(str,x)),qqs);
-  return ':'.join([name]+map(str,args)+qqs);
-
 class NodeScope (object):
-  class UnqualNode (object):
-    """An UnqualNode is a placeholder object returned by 'ns.name'. Once qualified with the () operator
-    (or initialized via <<), it becomes a fully-fledged _NodeRepr registered in the repository.""";
-    __slots__ = [ "name","scope","_debuginfo" ];
-    def __init__ (self,name,scope):  
-      (self.name,self.scope) = (name,scope);
-      self._debuginfo = "%s(%s)" % (name,_identifyCaller());
-    def __call__ (self,*quals,**kwquals):
-      return self.scope.QualifyNode(self.name,*quals,**kwquals);
-    def __lshift__ (self,arg):
-      arg = _NodeSpec.resolve(arg);
-      if arg is None:
-        raise TypeError,self._debuginfo+": cannot << something of type "+type(arg).__name__;
-      # doing a << on an unqualified node implictly applies empty qualifiers to it. Which we do here, but
-      # we must also replace the UnqualNode entry in out NodeScope with the qualified one in case
-      # it is accessed later.
-      node = self.scope.QualifyNode(self.name);
-      self.scope.__dict__[self.name] = node << arg;
-      return node;
-      
   def __init__ (self,name=None,parent=None,*quals,**kwquals):
     if name is None:
       if quals:
@@ -402,16 +369,16 @@ class NodeScope (object):
     self.ROOT = NodeGroup();
       
   def __getattr__ (self,name):
-    try: return self.__dict__[name];
-    except KeyError: pass;
-    _dprint(5,'creating unqual node',name);
-    if self._name is None:
-      nodename = name;
-    else:
-      nodename = '/'.join((self._name,name));
-    node = self.UnqualNode(nodename,self);
-    _dprint(5,'inserting node',name,'into our attributes');
-    self.__dict__[name] = node;
+    try: node = self.__dict__[name];
+    except KeyError: 
+      _dprint(5,'node',name,'not found, creating stub for it');
+      if self._name is None:
+        nodename = name;
+      else:
+        nodename = '/'.join((self._name,name));
+      node = _NodeStub(nodename,nodename,self);
+      _dprint(5,'inserting node stub',name,'into our attributes');
+      self.__dict__[name] = node;
     return node;
   __getitem__ = __getattr__;
   
@@ -427,18 +394,12 @@ class NodeScope (object):
       name = qualifyName(name0,count);
       count += 1;
     # create the node
-    node = _NodeRepr(name,self,name,(),{});
-    self._repository[name] = node;
+    node = _NodeStub(name,name,self);
+    # bind node, this also adds it to the repository
+    node << _Meq.Constant(value=value);
+    # add to map of constants 
     self._constants[value] = node;
-    return node << _Meq.Constant(value=value);
-    
-  def QualifyNode (self,name,*quals,**kwquals):
-    """Returns a _NodeRepr object corresponding to a qualified node.""";
-    fqname = qualifyName(name,*quals,**kwquals);
-    node = self._repository.get(fqname,None);
-    if node is None:
-      node = self._repository[fqname] = _NodeRepr(fqname,self,name,quals,kwquals);
-    return node;
+    return weakref.proxy(node);
     
   def Repository (self):
     """Returns the repository""";
@@ -466,3 +427,47 @@ class NodeScope (object):
     """;
     return self._repository.rootmap();
 
+
+# helper func to qualify a name
+def qualifyName (name,*args,**kws):
+  """Qualifies name by appending a dict of qualifiers to it, in the form
+  of name:a1:a2:k1=v1:k2=v2, etc."""
+  qqs = list(kws.iteritems());
+  qqs.sort();
+  qqs = map(lambda x: '='.join(map(str,x)),qqs);
+  return ':'.join([name]+map(str,args)+qqs);
+
+# used to generate Meq.Constants and such
+_Meq = ClassGen('Meq');
+
+# helper func to identify original caller of a function
+def _identifyCaller (depth=3,skip_internals=True):
+  """Identifies source location from which function was called.
+  Normal depth is 3, corresponding to the caller of the caller of
+  _identifyCaller(), but if skip_internals=True, this will also 
+  skip over calls to built-ins.""";
+  stack = traceback.extract_stack();
+  if skip_internals:
+    for frame in stack[-depth::-1]:
+      (filename,line,funcname,text) = frame;
+      if not filename.endswith('TDLimpl.py'):
+        break;
+    # got to the end of the stack without finding a non-built-in? Fall back 
+    # to starting frame
+    if funcname.startswith('__'):
+      (filename,line,funcname,text) = stack[-depth];
+  else:
+    (filename,line,funcname,text) = stack[-depth];
+  filename = filename.split('/')[-1];
+  return "%s:%d:%s" % (filename,line,funcname);
+  
+# helper func to pretty-print a node
+def _printNode (node,name='',offset=0):
+  header = (' ' * offset);
+  if name:
+    header += name+": ";
+  header += str(node);
+  print "%s: %.*s" % (header,78-len(header),str(node._initrec));
+  for (ich,child) in node.children:
+    _printNode(child,str(ich),offset+2);
+    
