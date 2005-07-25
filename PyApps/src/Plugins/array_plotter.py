88cc30ab3423ca22425deb7f357754bb407bd273
Tony Willis <twillis@drao.nrc.ca>
2005-07-25 18:48:26 +0000

    %[ER: ]% added a layout widget so that color bars can be displayed

--- PyApps/src/Plugins/array_plotter.py
+++ PyApps/src/Plugins/array_plotter.py
@@ -10,8 +10,11 @@ from Timba import Grid
 
 from Timba.Plugins.display_image import *
 
-from qt import *
 from numarray import *
+from qt import *
+from qwt import *
+from QwtPlotImage import *
+from QwtColorBar import *
 
 from vtk_qt_3d_display import *
 
@@ -28,13 +31,33 @@ class ArrayPlotter(GriddedPlugin):
   def __init__(self,gw,dataitem,cellspec={},**opts):
     GriddedPlugin.__init__(self,gw,dataitem,cellspec=cellspec);
 
-# create the plotter
-    if dataitem.data.rank < 3:
-      self._plotter = QwtImagePlot('spectra', self.wparent())
+# now create plotter and colorbar
+
+# first figure out the actual rank of the array we are plotting
+    self.rank = 0
+    shape = dataitem.data.shape
+    for i in range(len(shape)):
+      if shape[i] > 1:
+        self.rank = self.rank + 1
+    self.layout = None
+    if self.rank < 3:
+      if self.rank == 2:
+        self.layout = QHBox(self.wparent())
+        self.colorbar =  QwtColorBar(parent=self.layout)
+        self.colorbar.setRange(-1,1)
+        self.colorbar.hide()
+        self._plotter = QwtImageDisplay('spectra',parent=self.layout)
+        QObject.connect(self._plotter, PYSIGNAL('image_range'), self.colorbar.setRange) 
+        QObject.connect(self._plotter, PYSIGNAL('display_type'), self.colorbar.setDisplayType) 
+      else:
+        self._plotter = QwtImageDisplay('spectra',parent=self.wparent())
     else:
       self._plotter = vtk_qt_3d_display(self.wparent())
     self._plotter.show()
-    self.set_widgets(self._plotter,dataitem.caption,icon=self.icon());
+    if self.layout is None:
+      self.set_widgets(self._plotter,dataitem.caption,icon=self.icon());
+    else:
+      self.set_widgets(self.layout,dataitem.caption,icon=self.icon());
 
     if dataitem and dataitem.data is not None:
       self.set_data(dataitem);
@@ -43,12 +66,13 @@ class ArrayPlotter(GriddedPlugin):
 #    print "in destructor"
                                                                                            
   def wtop (self):
-    return self._plotter
+    return self.layout
 
   def set_data (self,dataitem,default_open=None,**opts):
     """ this function is the callback interface to the meqbrowser and
         handles new incoming data """
 
+# pass array to the plotter
     self._plotter.array_plot('data', dataitem.data)
 
 # enable & highlight the cell
