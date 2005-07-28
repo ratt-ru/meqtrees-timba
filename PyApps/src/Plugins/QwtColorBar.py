3731c9a14e28fbb35ad4973504ca48554424c6c7
Tony Willis <twillis@drao.nrc.ca>
2005-07-28 21:01:50 +0000

    %[ER: ]% Added help facility. Also added 'zoom' capability that can be used to select range of intensity that is displayed in the corresponding image.

--- PyApps/src/Plugins/QwtColorBar.py
+++ PyApps/src/Plugins/QwtColorBar.py
@@ -3,13 +3,20 @@
 # an adaption of the PyQwt-4.2 QwtImagePlotDemo.py example so that
 # we can draw color bars
 
+from Timba.GUI.pixmaps import pixmaps
 from QwtPlotImage import *
 from printfilter import *
+
+colorbar_instructions = \
+'''The colorbar displays the current range of intensities in the corresponding image display. You can interact with the colorbar to change the range of intensities displayed in the image.<br><br>
+Button 1 (Left): If you click the <b>left</b> mouse button on a location inside the colorbar and then drag it, a rectangular square will be seen. When you release the left mouse button, the range of intensity defined in the vertical (Y) direction will now specify the maximum range of intensity that will be shown in the image display. Image pixels with values greater or less than the selected range will be plotted with the maximum or minimum allowed values. The color rainbow or grayscale will always cover the specified range of pixels, so you can obtain increased detail by zooming in on an intensity range.<br><br>
+Button 2 (Right):Clicking the <b>right</b> mouse button in the colorbar window will cause a context menu with the option to 'unzoom intensity range' to appear. If you click on this menu item, then the colorbar scale (and the image scale) is reset to the intrinsic range associated with the current image.'''
 
 class QwtColorBar(QwtPlot):
 
     def __init__(self, plot_key=None, parent=None):
         QwtPlot.__init__(self, plot_key, parent)
+        self._mainwin = parent and parent.topLevelWidget()
 
 	# make a QwtPlot widget
         self.plotLayout().setMargin(0)
@@ -25,6 +32,8 @@ class QwtColorBar(QwtPlot):
         self.plotImage = QwtPlotImage(self)
         self.min = 0.0
         self.max = 256.0
+        self.image_min = None
+        self.image_max = None
         self.bar_array = reshape(arange(self.max), (1,256))
         self.y_scale = arange(2)
         self.y_scale = (self.min, self.max)
@@ -43,14 +52,30 @@ class QwtColorBar(QwtPlot):
         self.connect(self,
                      SIGNAL('plotMouseReleased(const QMouseEvent&)'),
                      self.onMouseReleased)
+
+        # add intructions on how to use
+        QWhatsThis.add(self, colorbar_instructions)
+
+        # create pull_down menu  - needed to over-ride Oleg's default
+        if self._mainwin:
+          self._menu = QPopupMenu(self._mainwin);
+          zoom = QAction(self);
+#         zoom.setIconSet(pixmaps.viewmag.iconset());
+          zoom.setText("unzoom intensity range");
+          zoom.addTo(self._menu);
+          QObject.connect(self._menu,SIGNAL("activated(int)"), self.unzoom)
         
         # replot
         self.replot()
     # __init__()
 
     def setRange(self, min, max):
-        self.min = min *1.0
-        self.max = max *1.0
+        self.min = min * 1.0
+        self.max = max * 1.0
+        if self.image_min is None:
+          self.image_min = self.min
+        if self.image_max is None:
+          self.image_max = self.max
         self.delta = (self.max - self.min) / 256.0
         for i in range (256):
           self.bar_array[0,i] = self.min + i * self.delta
@@ -59,6 +84,34 @@ class QwtColorBar(QwtPlot):
         self.show()
         self.replot()
     # set Range()
+
+    def setMaxRange(self, min, max):
+        self.image_min = min * 1.0
+        self.image_max = max * 1.0
+    # set Range()
+
+    def showDisplay(self, show_self):
+        if show_self > 0:
+          self.show()
+        else:
+          self.hide()
+        self.replot()
+    # showDisplay
+
+    def unzoom(self):
+        if len(self.zoomStack):
+          while len(self.zoomStack):
+            xmin, xmax, ymin, ymax = self.zoomStack.pop()
+          self.setAxisScale(QwtPlot.xBottom, xmin, xmax)
+          self.setAxisScale(QwtPlot.yLeft, self.image_min, self.image_max)
+          if self.image_min > self.image_max:
+            temp = self.image_max
+            self.image_max = self.image_min
+            self.image_min = temp
+          self.setRange(self.image_min, self.image_max)
+          self.emit(PYSIGNAL("set_image_range"),(self.image_min, self.image_max))
+        else:
+          return
 
     # set the type of colorbar display, can be one of "hippo", "grayscale" 
     # or "brentjens"
@@ -96,7 +149,8 @@ class QwtColorBar(QwtPlot):
                     self.axisScale(QwtPlot.yLeft).hBound(),
                     )
         elif Qt.RightButton == e.button():
-            self.zooming = 0
+            e.accept()
+            self._menu.popup(e.globalPos())
         # fake a mouse move to show the cursor position
         self.onMouseMoved(e)
     # onMousePressed()
@@ -125,6 +179,14 @@ class QwtColorBar(QwtPlot):
 
         self.setAxisScale(QwtPlot.xBottom, xmin, xmax)
         self.setAxisScale(QwtPlot.yLeft, ymin, ymax)
+        if ymin > ymax:
+          temp = ymax
+          ymax = ymin
+          ymin = temp
+        self.setRange(ymin, ymax)
+        self.emit(PYSIGNAL("set_image_range"),(ymin, ymax))
+
+
         self.replot()
     # onMouseReleased()
 
