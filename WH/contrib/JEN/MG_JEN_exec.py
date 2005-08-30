9a71bf7f91404efcec9340b92cf026670a5d812f
Jan Noordam <noordam@astron.nl>
2005-08-30 13:03:39 +0000

    I wrenched back control over this file

--- WH/contrib/JEN/MG_JEN_exec.py
+++ WH/contrib/JEN/MG_JEN_exec.py
@@ -16,6 +16,8 @@ script_name = 'MG_JEN_exec.py'
 from Timba.TDL import *
 from Timba.Meq import meq
 
+from copy import deepcopy
+
 from Timba.Contrib.JEN import MG_JEN_forest_state as MG_JEN_forest_state
 
 
@@ -82,7 +84,7 @@ def on_exit (ns, cc, name='_test_root'):
 #-----------------------------------------------------------------------------
 # Bundle the given nodes by making them children of a new node:
 
-def bundle (ns, cc, name='bundle', show_parent=True):
+def bundle (ns, cc, name='bundle', show_parent=False):
 	if not isinstance(cc, list): cc = [cc]
 	if len(cc) == 0:
 		parent = ns[name] << -1.23456789
@@ -108,8 +110,16 @@ def bundle (ns, cc, name='bundle', show_parent=True):
 	return parent
    
 
-#-------------------------------------------------------------------------------- 
+
+
+
+
+
+#================================================================================
+#================================================================================
+#================================================================================
 # The function that does the work for _test_forest()
+#================================================================================
 
 def meqforest (mqs, parent, request=None, **pp):
    pp.setdefault('trace', False)
@@ -117,15 +127,56 @@ def meqforest (mqs, parent, request=None, **pp):
    # Execute the meqforest with the specified (or default) request:
    request = make_request(request, **pp)
    global _test_root                                         # defined in .on_exit()
-   # mqs.meq('Debug.Set.Level',record(debug_level=100));
-   # mqs.meq('Node.Set.Breakpoint',record(name='solver:GJones:q=uvp',breakpoint=255));
-   mqs.meq('Node.Execute',record(name=_test_root, request=request));
+   # mqs.meq('Debug.Set.Level',record(debug_level=100))
+   # mqs.meq('Node.Set.Breakpoint',record(name='solver:GJones:q=uvp',breakpoint=255))
 
-   # Save the meqforest in a file:
+   # Needed for the moment, until OMS has figured out the threading:
+   # mqs.meq('Clear.Breakpoints',record(name='solver:GJones:q=uvp',breakpoint=255))
+
+   result = mqs.meq('Node.Execute',record(name=_test_root, request=request), wait=True)
+   # print 'result:',result
+   MG_JEN_forest_state.attach_test_result (result)
+
+   # Save the meqforest in a file (and perhapes a reference file, for auto-testing):
    pp.setdefault('save', True)       
-   if pp['save']: MG_JEN_forest_state.save(mqs)
+   pp.setdefault('save_reference', True)       
+   if pp['save']:
+	   MG_JEN_forest_state.save_meqforest(mqs, reference=pp['save_reference'])
    
-   return
+   return True
+
+
+#---------------------------------------------------------
+# Various possible commands (from MeqForest.g)
+
+# r := private.mqsv.meq('Clear.Cache', [name='MeqSolver', recursive=T], wait_reply=T)
+#      private.mqsv.meq('Clear.Funklets', wait_reply=T);
+#      private.mqsv.meq('Save.Forest', [file_name=tempfile], wait_reply=T);
+#      private.mqsv.meq('Clear.Forest', wait_reply=T);
+# Remove all funklets of the specified MeqParms:
+#      private.mqp.reset(mepname, parmname=parmname, trace=trace);
+# Load AFTER resetting the table....?
+#      private.mqsv.meq('Load.Forest', [file_name=tempfile], wait_reply=T);
+# r := private.mqsv.meq('Set.Forest.State', [state=state], wait_reply=T);
+# rr := private.mqsv.meq('Get.Forest.State', [=], wait_reply=T);
+# rr := private.mqsv.meq('Node.Get.State', [name=name], wait_reply=T);
+# r := private.mqsv.meq('Node.Set.State', [name=name, state=vv], wait_reply=T);
+# r := private.mqsv.meq ('Create.Node', cc, wait_reply=T, silent=F);
+#      private.mqsv.meq('Debug.Set.Level', [debug_level=100], wait_reply=T);
+# result := private.mqsv.meq('Node.Execute', rr, wait_reply=T);
+# ii := private.mqsv.meq('Get.Nodeindex', [name=defrec.name], wait_reply=T);
+# r := private.mqsv.meq('Resolve', [name=rname], wait_reply=T);
+#      private.mqsv.meq := function (command=F, opt=[=], wait_reply=F, silent=F) {
+# rr := [command=command, opt=opt, wait_reply=wait_reply, silent=silent];
+# else if (command=='Node.Set.BreakPoint') {
+# else if (command=='Node.Clear.BreakPoint') {
+# else if (command=='Node.Publish.Results') {
+# private.mqsv.resolve := function (name=F) {
+# private.mqsv.getnodestate := function (name=F) {
+# private.mqsv.getnodelist := function (children=T) {
+#   return [class="", name="", nodeindex=[], children=[=]];
+# private.mqsv.execute := function (name=F, request=F) {
+
 
 #---------------------------------------------------------
 # Helper function to make sure of a request:
@@ -186,7 +237,7 @@ def make_domain (domain=None, **pp):
       pp.setdefault('f1', 0)
       pp.setdefault('f2', 1)
       pp.setdefault('t1', 0)
-      pp.setdefault('t2', 10)
+      pp.setdefault('t2', 1)
       domain = meq.domain(pp['f1'],pp['f2'],pp['t1'],pp['t2'])
       if pp['trace']: print s,'pp =',pp
    if pp['trace']: print s,'-> domain =',domain
@@ -194,18 +245,30 @@ def make_domain (domain=None, **pp):
 
 
 
-#-------------------------------------------------------------------------------- 
+
+
+
+
+#================================================================================
 # Execute the script without a meqserver:
+#================================================================================
 
 def without_meqserver(script_name='<script_name>'):
 	ns = NodeScope();
 	_define_forest(ns);
 	ns.Resolve();
+        global _test_root                                     # defined in .on_exit()
+        display_subtree (ns[_test_root], script_name)
         display_nodescope(ns, script_name)
 	return 
 
 
-# Used by .without_meqserver(): 
+
+#================================================================================
+# Some useful display functions:
+#================================================================================
+
+#--------------------------------------------------------------------------------
 
 def display_nodescope (ns, txt='<txt>', trace=1):
    print '\n*** display of NodeScope (',txt,'):'
@@ -236,19 +299,21 @@ def display_nodescope (ns, txt='<txt>', trace=1):
    print '** - dir(ns) -> ',dir(ns)
    
    print '**'
-   # JEN_display (ns.AllNodes(),'ns.AllNodes()', full=1)
+   display_object (ns.AllNodes(), 'ns.AllNodes()')
    print '** - ns.AllNodes() : ',type(ns.AllNodes()),'[',len(ns.AllNodes()),']'
    print '** - ns.Repository() : ',type(ns.Repository()),'[',len(ns.Repository()),']'
    print '** - ns.RootNodes() : ',type(ns.RootNodes()),'[',len(ns.RootNodes()),']'
    print '** - ns.RootNodes() -> ',ns.RootNodes()
-   # JEN_display (ns.RootNodes(),'ns.RootNodes()', full=1)
+   display_object (ns.RootNodes(), 'ns.RootNodes()')
    root = ns.RootNodes()
-   for key in root.keys(): display_subtree (root[key],'root['+key+']', full=1)
+   for key in root.keys(): display_subtree (root[key],'root['+key+']', full=False)
       
    print '**'
    print '** - ns.__doc__ -> ',ns.__doc__
    print '*** End of NodeScope ()\n'
    return
+
+
 
 #----------------------------------------------------------------------------------
 # Recursively display the subtree underneath a NodeStub object (node):
@@ -259,9 +324,12 @@ def display_subtree (node, txt='<txt>', level=0,
    total = '_total_count'
    klasses = '_classes'
    if level == 0:
+      initrec = deepcopy(node.initrec())
+      for field in ['name','class','defined_at']:
+          if initrec.has_key(field): initrec.__delitem__(field)
       print
       print '** TDL subtree (',txt,') ( recurse =',recurse,'):'
-      print level,indent,node,'  ',node.initrec()
+      print level,indent,node,'  ',initrec
       if not full: print '   (use full=1 to display the subtree itself)'
       key = str(node)
       count = {}
@@ -293,11 +361,14 @@ def display_subtree (node, txt='<txt>', level=0,
          klass = node[1].classname
          if not count[klasses].has_key(klass): count[klasses][klass] = 0
          count[klasses][klass] += 1
-         rr = node[1].initrec()
-         if len(rr.keys()) > 1:
-            rr = rr.copy()
-            rr.__delitem__('class')
-            if full: print '  ',rr,
+         initrec = deepcopy(node[1].initrec())
+         if len(initrec.keys()) > 1:
+	    for field in ['name','class','defined_at']:
+	       if initrec.has_key(field): initrec.__delitem__(field)
+            if initrec.has_key('default_funklet'):
+               coeff = initrec.default_funklet.coeff
+               initrec.default_funklet.coeff = [coeff.shape,coeff.flat]
+            if full: print '  ',initrec,
          if recurse>0:
             if full: print
             for j in range(len(node[1].stepchildren)):
@@ -310,6 +381,91 @@ def display_subtree (node, txt='<txt>', level=0,
           
          else:
             if full: print '      (further recursion inhibited)'
+
+
+
+#----------------------------------------------------------------------------------
+# Display any Python object(v):
+
+def display_object (v, name='<name>', txt='', full=0, indent=0):
+    if indent==0: print '\n** display of Python object:',name,': (',txt,'):'
+    print '**',indent*'.',name,':',
+    
+    if isinstance(v, (str, list, tuple, dict, record)):
+        # sizeable types (otherwise, len(v) gives an error):
+        vlen = len(v)
+        slen = '['+str(vlen)+']'
+
+        if isinstance(v, str):
+            print 'str',slen,
+            print '=',v
+      
+        elif isinstance(v, list):
+            print 'list',slen,
+            separate = False
+            types = {}
+            for i in range(vlen):
+                stype = str(type(v[i]))
+                types[stype] = 1
+                s1 = stype.split(' ')
+                if s1[0] == '<class': separate = True
+                if isinstance(v[i], (dict, record)): separate = True
+            if len(types) > 1: separate = True
+
+            if separate:
+                print ':'
+                for i in range(vlen): display_object (v[i], '['+str(i)+']', indent=indent+2)
+            elif vlen == 1:
+                print '=',[v[0]]
+            elif vlen < 5:
+                print '=',v
+            else:
+                print '=',[v[0],'...',v[vlen-1]]
+
+        elif isinstance(v, tuple):
+            print 'tuple',slen,
+            print '=',v
+          
+        elif isinstance(v, (dict, record)):
+            if isinstance(v, record):
+                print 'record',slen,':'
+            elif isinstance(v, dict):
+                print 'dict',slen,':'
+            keys = v.keys()
+            n = len(keys)
+            types = {}
+            for key in keys: types[str(type(v[key]))] = 1
+            if len(types) > 1:
+                for key in v.keys(): display_object (v[key], key, indent=indent+2)
+            elif n < 10:
+                for key in v.keys(): display_object (v[key], key, indent=indent+2)
+            elif full:
+                for key in v.keys(): display_object (v[key], key, indent=indent+2)
+            else:
+                for key in [keys[0]]: display_object (v[key], key, indent=indent+2)
+                if n > 20:
+                    print '**',(indent+2)*' ','.... (',n-2,'more fields of the same type )'
+                else:
+                    print '**',(indent+2)*' ','.... ( skipped keys:',keys[1:n-1],')'
+                for key in [keys[n-1]]: display_object (v[key], key, full=full, indent=indent+2) 
+        
+
+        else: 
+            print type(v),'=',v
+
+    else: 
+        # All other types:
+        print type(v),'=',v
+
+    if indent == 0: print
+
+
+
+
+
+
+
+
 
 
 
@@ -339,36 +495,33 @@ def _test_forest (mqs, parent):
 # Test routine to check the tree for consistency in the absence of a server
 
 if __name__ == '__main__':
-   if True:
-      # This is the default
-      without_meqserver(script_name)
+   print '\n****************\n** Local test of:',script_name,':\n'
+
+   # Generic test:
+   without_meqserver(script_name)
    
-   else:
-      # Some local tests
-      print '\n**',script_name,':\n'
-      if 0:
-         domain = make_domain(trace=True)
-         domain = make_domain('21cm', trace=True)
-         domain = make_domain('lofar', trace=True)
-         domain = make_domain('lofar', f1=10, trace=True)
-         domain = make_domain(domain, trace=True)
-         print
+   # Various local tests:
+   if 1:
+      domain = make_domain(trace=True)
+      domain = make_domain('21cm', trace=True)
+      domain = make_domain('lofar', trace=True)
+      domain = make_domain('lofar', f1=10, trace=True)
+      domain = make_domain(domain, trace=True)
+      display_object (domain, 'domain', 'MG_JEN_exec')
+      
+   if 1:
+      # cells = make_cells(trace=True)
+      cells = make_cells(domain='lofar', f1=10, trace=True)
+      cells = make_cells(cells, trace=True)
+      display_object (cells, 'cells', 'MG_JEN_exec')
+      
+   if 1:
+      # request = make_request(trace=True)
+      request = make_request(domain='lofar', trace=True)
+      request = make_request(request, trace=True)
+      display_object (request, 'request', 'MG_JEN_exec')
 
-      if 0:
-         # cells = make_cells(trace=True)
-         cells = make_cells(domain='lofar', f1=10, trace=True)
-         cells = make_cells(cells, trace=True)
-         print
-
-      if 0:
-         # request = make_request(trace=True)
-         request = make_request(domain='lofar', trace=True)
-         request = make_request(request, trace=True)
-         print
-
-      print '\n** end of',script_name,'\n'
-
-
+   print '\n** End of local test of:',script_name,'\n*************\n'
 
 
 #********************************************************************************
