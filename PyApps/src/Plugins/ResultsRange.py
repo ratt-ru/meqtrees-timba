bdcc44f95fb04103094d94bccae3b96f5d84b51e
Tony Willis <twillis@drao.nrc.ca>
2006-02-09 00:20:35 +0000

    Adapted to follow suggestion of Maaijke about display of rq id and of Oleg for having spinbox and slider side by side

--- PyApps/src/Plugins/ResultsRange.py
+++ PyApps/src/Plugins/ResultsRange.py
@@ -4,8 +4,9 @@
 # displays
 
 # modules that are imported
-from qt import *
 import sys
+from qt import *
+from BufferSizeDialog import *
 
 # The ResultsRange class is directly adapted from the Qt/PyQt 
 # tutorial code examples.
@@ -14,45 +15,96 @@ import sys
 
 class ResultsRange(QWidget):
     def __init__(self, parent=None, name=None):
-        QWidget.__init__(self, parent, name)
+      QWidget.__init__(self, parent, name)
 
-        self.maxVal = 9
-        self.spinbox = QSpinBox(self)
-        self.spinbox.setMinValue(0)
-        self.spinbox.setMaxValue(self.maxVal)
-        self.spinbox.setWrapping(True)
+      self. menu_table = {
+      'Adjust results buffer size': 301,
+      }
 
-        self.slider = QSlider(Qt.Horizontal, self, "slider")
-        self.slider.setTickmarks(QSlider.Below)
-        self.slider.setTickInterval(1)
-        self.slider.setRange(0, self.maxVal)
+      self.menu = None
+      self.maxVal = 10
+      self.label_info = QLabel('', self)
+      self.label_info1 = QLabel('    ', self)
+      self.spinbox = QSpinBox(self)
+      self.spinbox.setMinValue(1)
+      self.spinbox.setMaxValue(self.maxVal)
+      self.spinbox.setWrapping(True)
 
-        self.setValue()
+      self.slider = QSlider(Qt.Horizontal, self, "slider")
+      self.slider.setTickmarks(QSlider.Below)
+      self.slider.setTickInterval(1)
+      self.slider.setRange(1, self.maxVal)
 
-        self.connect(self.slider, SIGNAL("valueChanged(int)"), self.update_slider)
-        self.connect(self.spinbox, SIGNAL("valueChanged(int)"), self.update_spinbox)
 
-        self.layout = QGridLayout(self)
-        self.layout.addWidget(self.spinbox, 0,0)
-        self.layout.addWidget(self.slider, 1,0)
+      self.setValue()
+
+      self.connect(self.slider, SIGNAL("valueChanged(int)"), self.update_slider)
+      self.connect(self.spinbox, SIGNAL("valueChanged(int)"), self.update_spinbox)
+
+      self.layout = QHBoxLayout(self)
+      spacer = QSpacerItem(22,9,QSizePolicy.Expanding,QSizePolicy.Minimum)
+      spacer1 = QSpacerItem(11,9,QSizePolicy.Expanding,QSizePolicy.Minimum)
+      self.layout.addItem(spacer)
+      self.layout.addWidget(self.label_info)
+      self.layout.addWidget(self.spinbox)
+      self.layout.addWidget(self.label_info1)
+      self.layout.addWidget(self.slider)
+
+    def setLabel(self, string_value= ''):
+      self.label_info.setText(string_value + ' ') 
+
+    def setMaxValue(self, max_value= 0):
+      self.maxVal = max_value
+      self.slider.setRange(1, self.maxVal)
+      self.spinbox.setMaxValue(self.maxVal)
 
     def setValue(self, value= 0):
-        self.slider.setValue(value)
-        self.spinbox.setValue(value)
+      self.slider.setValue(value)
+      self.spinbox.setValue(value)
+      self.initContextmenu()
 
-    def setRange(self, max_range):
-        self.maxVal = max_range-1
-        self.slider.setRange(0, self.maxVal)
-        self.spinbox.setMaxValue(self.maxVal)
-        self.setValue(self.maxVal)
+    def setRange(self, range_value):
+      if range_value <= self.maxVal:
+        self.slider.setRange(1, range_value)
+        self.spinbox.setMaxValue(range_value)
+        self.setValue(range_value)
 
     def update_slider(self, slider_value):
-        self.spinbox.setValue(slider_value)
-        self.emit(PYSIGNAL("result_index"),(slider_value,))
+      self.spinbox.setValue(slider_value)
+      self.emit(PYSIGNAL("result_index"),(slider_value-1,))
 
     def update_spinbox(self, spin_value):
-        self.slider.setValue(spin_value)
-        self.emit(PYSIGNAL("result_index"),(spin_value,))
+      self.slider.setValue(spin_value)
+      self.emit(PYSIGNAL("result_index"),(spin_value-1,))
+
+    def initContextmenu(self):
+      """Initialize the result buffer context menu
+      """
+      if self.menu is None:
+        self.menu = QPopupMenu(self)
+        toggle_id = self.menu_table['Adjust results buffer size']
+        self.menu.insertItem("Adjust results buffer size", toggle_id)
+        QObject.connect(self.menu,SIGNAL("activated(int)"),self.handleMenu)
+
+    def setResultsBuffer(self, result_value):
+      if result_value < 0:
+        return
+      self.maxVal = result_value
+      self.slider.setRange(1, self.maxVal)
+      self.spinbox.setMaxValue(self.maxVal)
+      self.emit(PYSIGNAL("adjust_results_buffer_size"),(result_value,))
+
+    def handleMenu(self, menuid):
+        if menuid == self.menu_table['Adjust results buffer size']:
+          results_dialog = BufferSizeDialog(self.maxVal, self)
+          QObject.connect(results_dialog,PYSIGNAL("return_value"),self.setResultsBuffer)
+
+          results_dialog.show()
+
+    def mousePressEvent(self, e):
+      if Qt.RightButton == e.button():
+        e.accept()
+        self.menu.popup(e.globalPos());
 
 # the following tests the ResultsRange class
 def make():
