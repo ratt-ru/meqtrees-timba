843bbd39aa768c1b8e997ca7bb759d69976ab55e
Sarod Yatawatta <yatawatta@astron.nl>
2006-02-22 18:00:52 +0000

    added some documentation

--- LSM/src/LSM/__init__.py
+++ LSM/src/LSM/__init__.py
@@ -1,1 +1,5 @@
-#This directory contains source code of the LSM.
+# This directory contains source code of the LSM.
+# If you need to use the LSM, the only file you should have a look at is LSM.
+# The rest of the files are either used internally by the LSM or by the 
+# GUI of the LSM.
+# In the file LSM(.py) you have two classes: LSM and PUnit.
