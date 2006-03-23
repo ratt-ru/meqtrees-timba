ff42c6c4290ad78d0f61a93d0ea68b5ed7d8bb6f
Oleg Smirnov <osmirnov@gmail.com>
2006-03-23 20:58:41 +0000

    Added spectral indices

--- WH/contrib/AGW/clar_fast_predict.py
+++ WH/contrib/AGW/clar_fast_predict.py
@@ -8,7 +8,7 @@ import os
 # MS name
 msname = "TEST_CLAR_27-4800.MS";
 # number of timeslots to use at once
-tile_size = 120
+tile_size = 60
 
 # MS input queue size -- must be at least equal to the no. of ifrs
 input_queue_size = 500
@@ -49,6 +49,19 @@ Settings.forest_state = record(bookmarks=[
 #   record(viewer="Result Plotter",udi="/node/stokes:Q:3C343",pos=(1,0)),
 #    record(viewer="Result Plotter",udi="/node/stokes:Q:3C343_1",pos=(1,1)),
 #   record(viewer="Result Plotter",udi="/node/solver",pos=(1,1)),
+  ]),
+  record(name='Source fluxes',page=[
+    record(viewer="Result Plotter",udi="/node/stokes:I:src_1",pos=(0,0)),
+    record(viewer="Result Plotter",udi="/node/stokes:I:src_2",pos=(0,1)),
+    record(viewer="Result Plotter",udi="/node/stokes:I:src_3",pos=(0,2)),
+    record(viewer="Result Plotter",udi="/node/stokes:I:src_4",pos=(1,0)),
+    record(viewer="Result Plotter",udi="/node/stokes:I:src_5",pos=(1,1)),
+    record(viewer="Result Plotter",udi="/node/stokes:I:src_6",pos=(1,2)),
+    record(viewer="Result Plotter",udi="/node/stokes:I:src_7",pos=(2,0)),
+    record(viewer="Result Plotter",udi="/node/stokes:I:src_8",pos=(2,1)),
+    record(viewer="Result Plotter",udi="/node/stokes:I:src_9",pos=(2,2)),
+    record(viewer="Result Plotter",udi="/node/stokes:I:src_10",pos=(3,0)),
+    record(viewer="Result Plotter",udi="/node/width_fq",pos=(3,1)),
   ]) \
 ]);
 
@@ -63,12 +76,14 @@ class PointSource:
     def __init__(self, name='', ra=0.0, dec=0.0,
                  I=0.0, Q=0.0, U=0.0, V=0.0,
                  Iorder=0, Qorder=0, Uorder=0, Vorder=0,
+                 spi=0.0,
                  table=''):
         self.name   = name
         self.ra     = ra
         self.dec    = dec
         self.IQUV   = array([I,Q,U,V])
         self.IQUVorder = array([Iorder,Qorder,Uorder,Vorder])
+        self.spi    = spi
         self.table  = table
         pass
     pass
@@ -78,42 +93,52 @@ def create_source_model(tablename=''):
     source_model = []
     source_model.append( PointSource(name="src_1",I=1.0, Q=0.0, U=0.0, V=0.0,
                     Iorder=0, ra=0.030272885, dec=0.575762621,
+                    spi=-0.71,
                     table=tablename))
 
     source_model.append( PointSource(name="src_2",  I=1.0, Q=0.0, U=0.0, V=0.0,
                     Iorder=0, ra=0.0306782675, dec=0.575526087,
+                    spi=-0.62,
                     table=tablename))
 
     source_model.append( PointSource(name="src_3",I=1.0, Q=0.0, U=0.0, V=0.0,
                     Iorder=0, ra=0.0308948646, dec=0.5762655,
+                    spi=-0.53,
                     table=tablename))
 
     source_model.append( PointSource(name="src_4",I=1.0, Q=0.0, U=0.0, V=0.0,
                     Iorder=0, ra=0.0308043705, dec=0.576256621,
+                    spi=-0.44,
                     table=tablename))
 
     source_model.append( PointSource(name="src_5",I=1.0, Q=0.0, U=0.0, V=0.0,
                     Iorder=0, ra=0.030120036, dec=0.576310965,
+                    spi=-0.35,
                     table=tablename))
 
     source_model.append( PointSource(name="src_6",I=1.0, Q=0.0, U=0.0, V=0.0,
                     Iorder=0, ra=0.0301734016, dec=0.576108805,
+                    spi=-0.26,
                     table=tablename))
 
     source_model.append( PointSource(name="src_7",I=1.0, Q=0.0, U=0.0, V=0.0,
                     Iorder=0, ra=0.0302269161, dec=0.576333355,
+                    spi=-0.17,
                     table=tablename))
 
     source_model.append( PointSource(name="src_8",I=1.0, Q=0.0, U=0.0, V=0.0,
                     Iorder=0, ra=0.0304215356, dec=0.575777607,
+                    spi=-0.28,
                     table=tablename))
 
     source_model.append( PointSource(name="src_9",I=1.0, Q=0.0, U=0.0, V=0.0,
                     Iorder=0, ra=0.0307416105, dec=0.576347166,
+                    spi=-0.39,
                     table=tablename))
 
     source_model.append( PointSource(name="src_10",I=1.0, Q=0.0, U=0.0, V=0.0,
                     Iorder=0, ra=0.0306878027, dec=0.575851951,
+                    spi=-0.40,
                     table=tablename))
 
     return source_model
@@ -250,11 +275,14 @@ def create_constant_nodes(ns):
     ns.one << Meq.Constant(1.0)
     ns.half << Meq.Constant(0.5)
     ns.ln_16 << Meq.Constant(-2.7725887)
+    ns.freq << Meq.Freq;
+    # f1/f0 is used for beam widths and spectral indices
+    ns.freq_ratio << ns.freq / ref_frequency;
 
     # beam width at reference frequency
     ns.width << Meq.Constant(beam_width);
     # this scales it with frequency
-    ns.width_fq << ns.width * ( Meq.Freq() / ref_frequency );
+    ns.width_fq << ns.width * ( ns.freq / ref_frequency );
     ns.width_sq << Meq.Sqr(ns.width_fq)
     
 def noise_matrix (stddev=0.1):
@@ -270,18 +298,25 @@ def noise_matrix (stddev=0.1):
 def forest_source_subtrees (ns, source):
   print 'source.name ', source.name
   IQUVpolcs =[None]*4
-  STOKES=["I","Q","U","V"]
+  STOKES=["I0","Q","U","V"]
   for (i,stokes) in enumerate(STOKES):
     if(source.IQUV[i] != None):
       print 'i source.IQUV[i] source.IQUVorder ', i, ' ', source.IQUV[i], ' ', source.IQUVorder[i]
       IQUVpolcs[i] = create_polc_ft(degree_f=source.IQUVorder[i], 
 					c00= source.IQUV[i])
       pass
-    st = ns.stokes(stokes, source.name) << Meq.Parm(IQUVpolcs[i],
+    st = ns.stokes(stokes,source.name) << Meq.Parm(IQUVpolcs[i],
 					table_name=source.table,
 					node_groups='Parm')
 #   create_refparms(st);
-    pass    
+    pass
+  ns.spi(source.name) << Meq.Parm(create_polc_ft(c00=source.spi),
+                               	  table_name=source.table,
+				  node_groups='Parm');
+                                  
+  ns.stokes("I",source.name) << \
+      ns.stokes("I0",source.name) * Meq.Pow(ns.freq_ratio,ns.spi(source.name));
+  
   ns.xx(source.name) << (ns.stokes("I",source.name)+ns.stokes("Q",source.name))*0.5
   ns.yx(source.name) << Meq.ToComplex(ns.stokes("U",source.name),ns.stokes("V",source.name))*0.5
   ns.xy(source.name) << Meq.Conj(ns.yx(source.name))
