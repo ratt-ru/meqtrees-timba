2ab40099d1852ef6f2d7ac11c283a76d599b18d7
Tony Willis <twillis@drao.nrc.ca>
2006-03-31 19:00:29 +0000

    Updated with Gerard Vermeulen's corrections for the 'draw' method

--- PyApps/src/Plugins/ComplexScaleDraw.py
+++ PyApps/src/Plugins/ComplexScaleDraw.py
@@ -1,7 +1,7 @@
 from qt import *
 from qwt import *
 
-class ComplexScaleDraw (QwtScaleDraw):
+class ComplexScaleDraw(QwtScaleDraw):
 
     def __init__(self,divisor):
         QwtScaleDraw.__init__(self)
@@ -14,139 +14,147 @@ class ComplexScaleDraw (QwtScaleDraw):
     def label(self,v):
 	v = v % self.divisor
         return QwtScaleDraw.label(self,v)
+    # label()
 
     def draw(self, painter):
         # 1. paint real value ticks as done in C++ code
         # 2. paint imag value ticks with the shift depending on
         #    the dimension and tick spacing
         # 3. draw backbone, if needed
-        # need to understand QwtScaleDiv
 
-        d_scldiv = QwtScaleDraw.scaleDiv(self)
-        d_majLen = QwtScaleDraw.majTickLength(self)
-        d_minLen = 4         # not directly accessible by a function call
-                             # use default as given in c++ equivalent
-        d_medLen = 6         # not directly accessible by a function call
-                             # use default as given in c++ equivalent
         step_eps = 1.0e-6    # constant value in the c++ equivalent
-        Backbone = True      # try out False and see what happens
+        scldiv = QwtScaleDraw.scaleDiv(self)
+        majLen, medLen, minLen = QwtScaleDraw.tickLength(self)
         self.offset = 0
-# plot major ticks
-        for i in range(d_scldiv.majCnt()):
-            val = d_scldiv.majMark(i)
+        # plot major ticks
+        for i in range(scldiv.majCnt()):
+            val = scldiv.majMark(i)
             v = val % self.divisor
             if self.offset == 0 and v != val:
-              self.offset = d_scldiv.majStep() - v % d_scldiv.majStep()
+              self.offset = scldiv.majStep() - v % scldiv.majStep()
             val = val + self.offset
-            QwtScaleDraw.drawTick(self,painter, val, d_majLen)
-            QwtScaleDraw.drawLabel(self,painter, val)
-# finally, in this case plot a major tick at the dividing point
-        QwtScaleDraw.drawTick(self,painter, self.divisor, d_majLen)
+            QwtScaleDraw.drawTick(self, painter, val, majLen)
+            QwtScaleDraw.drawLabel(self, painter, val)
+
+        # also, plot a major tick at the dividing point
+        QwtScaleDraw.drawTick(self,painter, self.divisor, majLen)
         QwtScaleDraw.drawLabel(self,painter,self.divisor)
 
-# can't handle logs properly, but so what?
-        if d_scldiv.logScale():
-            for i in range(d_scldiv.minCnt()):
-                QwtScaleDraw.drawTick(self,painter, d_scldiv.minMark(i), d_minLen)
+        # probably can't handle logs properly??
+        if scldiv.logScale():
+            for i in range(scldiv.minCnt()):
+                QwtScaleDraw.drawTick(
+                    self, painter, scldiv.minMark(i), minLen)
         else:
-            kmax = d_scldiv.majCnt() - 1
+            # do minor ticks
+            kmax = scldiv.majCnt() - 1
             if kmax > 0:
-                majTick = d_scldiv.majMark(0)
-                hval = majTick - 0.5 * d_scldiv.majStep()
+                majTick = scldiv.majMark(0)
+                hval = majTick - 0.5 * scldiv.majStep()
                 k = 0
-                for i in range(d_scldiv.minCnt()):
-                    val = d_scldiv.minMark(i)
+                for i in range(scldiv.minCnt()):
+                    val = scldiv.minMark(i)
                     if  val > majTick:
                         if k < kmax:
                             k = k + 1
-                            majTick = d_scldiv.majMark(k)
+                            majTick = scldiv.majMark(k)
                         else:
-                            majTick += d_scldiv.majMark(kmax) + d_scldiv.majStep()
-                        hval = majTick - 0.5 * d_scldiv.majStep()
+                            majTick += (scldiv.majMark(kmax)
+                                        + scldiv.majStep())
+                        hval = majTick - 0.5 * scldiv.majStep()
                     if val >= self.divisor:
                       val = val + self.offset
                     if val > 2 * self.divisor:
                       val = val - self.divisor
-                    if abs(val-hval) < step_eps * d_scldiv.majStep():
-                        QwtScaleDraw.drawTick(self,painter, val, d_medLen)
+                    if abs(val-hval) < step_eps * scldiv.majStep():
+                        QwtScaleDraw.drawTick(self,painter, val, medLen)
                     else:
-                        QwtScaleDraw.drawTick(self,painter, val, d_minLen)
+                        QwtScaleDraw.drawTick(self, painter, val, minLen)
 
-        if QwtScaleDraw.options(self) and Backbone:
-            QwtScaleDraw.drawBackbone(self,painter)
+        if QwtScaleDraw.options(self) & QwtScaleDraw.Backbone:
+            QwtScaleDraw.drawBackbone(self, painter)
 
+    # draw()
+# class ComplexScaleDraw()
 
-class ComplexScaleSeparate (QwtScaleDraw):
+class ComplexScaleSeparate(QwtScaleDraw):
 
-    def __init__(self,start_value, end_value):
+    def __init__(self, start_value, end_value):
         QwtScaleDraw.__init__(self)
         self.start_value = start_value
         self.end_value = end_value
         self.delta = self.end_value - self.start_value
+
     # __init__()
 
-    def label(self,v):
+    def label(self, v):
         if v >= self.end_value:
-          v = v - self.delta
-        return QwtScaleDraw.label(self,v)
+            v = v - self.delta
+        return QwtScaleDraw.label(self, v)
+
+    # label()
 
     def draw(self, painter):
         # 1. paint real value ticks as done in C++ code
         # 2. paint imag value ticks with the shift depending on
         #    the dimension and tick spacing
         # 3. draw backbone, if needed
-        # need to understand QwtScaleDiv
 
-        d_scldiv = QwtScaleDraw.scaleDiv(self)
-        d_majLen = QwtScaleDraw.majTickLength(self)
-        d_minLen = 4         # not directly accessible by a function call
-                             # use default as given in c++ equivalent
-        d_medLen = 6         # not directly accessible by a function call
-                             # use default as given in c++ equivalent
         step_eps = 1.0e-6    # constant value in the c++ equivalent
-        Backbone = True      # try out False and see what happens
+        scldiv = QwtScaleDraw.scaleDiv(self)
+        majLen, medLen, minLen = QwtScaleDraw.tickLength(self)
         self.offset = 0
-        self.divisor = self.end_value
-# plot major ticks
-        for i in range(d_scldiv.majCnt()):
-            val = d_scldiv.majMark(i)
-            v = val 
+        self.separator = self.end_value
+        # plot major ticks
+        for i in range(scldiv.majCnt()):
+            val = scldiv.majMark(i)
+            v = val
             if val >= self.end_value:
-              v = val - self.delta
+                v = val - self.delta
             if self.offset == 0 and v != val:
-              self.offset = d_scldiv.majStep() - v % d_scldiv.majStep()
+                self.offset = scldiv.majStep() - v % scldiv.majStep()
             val = val + self.offset
-            QwtScaleDraw.drawTick(self,painter, val, d_majLen)
-            QwtScaleDraw.drawLabel(self,painter, val)
+            QwtScaleDraw.drawTick(self, painter, val, majLen)
+            QwtScaleDraw.drawLabel(self, painter, val)
 
-# can't handle logs properly, but so what?
-        if d_scldiv.logScale():
-            for i in range(d_scldiv.minCnt()):
-                QwtScaleDraw.drawTick(self,painter, d_scldiv.minMark(i), d_minLen)
+        # also, plot a major tick at the dividing point
+        QwtScaleDraw.drawTick(self,painter, self.separator, majLen)
+        QwtScaleDraw.drawLabel(self,painter,self.separator)
+
+        # probably can't handle logs properly??
+        if scldiv.logScale():
+            for i in range(scldiv.minCnt()):
+                QwtScaleDraw.drawTick(
+                    self, painter, scldiv.minMark(i), minLen)
         else:
-            kmax = d_scldiv.majCnt() - 1
+            # do minor ticks
+            kmax = scldiv.majCnt() - 1
             if kmax > 0:
-                majTick = d_scldiv.majMark(0)
-                hval = majTick - 0.5 * d_scldiv.majStep()
+                majTick = scldiv.majMark(0)
+                hval = majTick - 0.5 * scldiv.majStep()
                 k = 0
-                for i in range(d_scldiv.minCnt()):
-                    val = d_scldiv.minMark(i)
+                for i in range(scldiv.minCnt()):
+                    val = scldiv.minMark(i)
                     if  val > majTick:
                         if k < kmax:
                             k = k + 1
-                            majTick = d_scldiv.majMark(k)
+                            majTick = scldiv.majMark(k)
                         else:
-                            majTick += d_scldiv.majMark(kmax) + d_scldiv.majStep()
-                        hval = majTick - 0.5 * d_scldiv.majStep()
+                            majTick += (scldiv.majMark(kmax)
+                                        + scldiv.majStep())
+                        hval = majTick - 0.5 * scldiv.majStep()
                     if val >= self.end_value:
-                      val = val + self.offset
-                    if val > 2 * self.divisor:
-                      val = val - self.divisor
-                    if abs(val-hval) < step_eps * d_scldiv.majStep():
-                        QwtScaleDraw.drawTick(self,painter, val, d_medLen)
+                        val = val + self.offset
+                    if val > 2 * self.separator:
+                        val = val - self.separator
+                    if abs(val-hval) < step_eps * scldiv.majStep():
+                        QwtScaleDraw.drawTick(self,painter, val, medLen)
                     else:
-                        QwtScaleDraw.drawTick(self,painter, val, d_minLen)
+                        QwtScaleDraw.drawTick(self, painter, val, minLen)
 
-        if QwtScaleDraw.options(self) and Backbone:
-            QwtScaleDraw.drawBackbone(self,painter)
+        if QwtScaleDraw.options(self) & QwtScaleDraw.Backbone:
+            QwtScaleDraw.drawBackbone(self, painter)
+
+    # draw()
+# class ComplexScaleSeparate()
 
