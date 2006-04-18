e07c2ccfb942a137de2a6eba1eac2558fb033195
Ger van Diepen <diepen@astron.nl>
2006-04-18 11:42:40 +0000

    BugID: 699 Added targets help, memcheck, and memcheckrun Use make check to do checkrun Allow in_* to be a directory

--- autoconf_share/runtest.sh
+++ autoconf_share/runtest.sh
@@ -89,7 +89,8 @@ if test -f "$srcdir/$1.in"; then
     \rm -f $1.in
     \cp $srcdir/$1.in  .
 fi
-\cp $srcdir/$1.in_* . > /dev/null 2>&1
+\rm -rf $1.in_*
+\cp -r $srcdir/$1.in_* . > /dev/null 2>&1
 if test -f "$srcdir/$1.stdout"; then
     \rm -f $1.stdout
     \cp $srcdir/$1.stdout .
@@ -123,5 +124,6 @@ $lfr_share_dir/assay $1 $MAXTIME $PREC
 STS=$?
 
 # Cleanup (mainly for make distcheck).
-\rm -f $1.stdout $1.run $1.in $1.parset $1.log_prop $1.in_*
+\rm -f $1.stdout $1.run $1.in $1.parset $1.log_prop
+\rm -rf $1.in_*
 exit $STS
