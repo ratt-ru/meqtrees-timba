607353f8e58692ead2634ff7eee476d82d24db72
Ronald Nijboer <rnijboer@astron.nl>
2006-07-24 13:38:53 +0000

    updated version

--- WH/contrib/RJN/MG_RJN_testUVBrick.py
+++ WH/contrib/RJN/MG_RJN_testUVBrick.py
@@ -21,6 +21,29 @@
 #   and the baseline under consideration.
 # In the test_forest function the Frequency / Time domain is defined
 
+#% $Id$ 
+
+#
+# Copyright (C) 2006
+# ASTRON (Netherlands Foundation for Research in Astronomy)
+# and The MeqTree Foundation
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
 # Get the Sixpack definition from RJN. 
 from Timba.Contrib.RJN import RJN_sixpack
 
@@ -30,11 +53,7 @@ import math
 import random
 
 # for Bookmarks and more (UVW) get JEN's forest state script
-from Timba.Contrib.JEN import MG_JEN_forest_state
-
-# Get some of JEN's UVW and LMN definitions
-from Timba.Trees import TDL_Cohset
-from Timba.Trees import TDL_radio_conventions
+from Timba.Trees import JEN_bookmarks
 
 # Get TDL and Meq for the Kernel
 from Timba.TDL import * 
@@ -100,26 +119,48 @@ def _define_forest(ns):
 
 
  # Construction of a Phase Center Two-Pack (RA, DEC)
- # For now this is done manually
- #
  # RA and Dec coordinates of the Patch Phase Center
- #  (based on 3C343.1)
  RA_0 = 4.35664870004
  Dec_0 = 1.09220644132
+ ra0 = ns.ra0 << RA_0;
+ dec0 = ns.dec0 << Dec_0;
+ radec0 = ns.radec0 << Meq.Composer(ra0,dec0);
+ # Initialize the name list for the PatchComposer node
+ child_rec = [radec0];
+ pc_name='Phase Center';
 
- pc_name = 'Phase Center';
- twoname = 'twopack['+pc_name+']';
+ # Construction of a single baseline
+ # Reference position
+ X0 = 0.0;
+ Y0 = 0.0;
+ Z0 = 0.0;
+ x0 = ns.x0 << X0;
+ y0 = ns.y0 << Y0;
+ z0 = ns.z0 << Z0;
+ xyz0 = ns.xyz0 << Meq.Composer(x0,y0,z0);
+ # Station 1
+ X1 = 0.0;
+ Y1 = 2700.0;
+ Z1 = 0.0;
+ x1 = ns.x(s1=1) << X1;
+ y1 = ns.y(s1=1) << Y1;
+ z1 = ns.z(s1=1) << Z1;
+ xyz1 = ns.xyz(s1=1) << Meq.Composer(x1,y1,z1);
+ uvw1 = ns.uvw(s1=1) << Meq.UVW(radec=radec0,xyz_0=xyz0,xyz=xyz1);
+ # Station 2
+ X2 = 0.0;
+ Y2 = 0.0;
+ Z2 = 0.0;
+ x2 = ns.x(s1=2) << X2;
+ y2 = ns.y(s1=2) << Y2;
+ z2 = ns.z(s1=2) << Z2;
+ xyz2 = ns.xyz(s1=2) << Meq.Composer(x2,y2,z2);
+ uvw2 = ns.uvw(s1=2) << Meq.UVW(radec=radec0,xyz_0=xyz0,xyz=xyz2);
+ # Baseline
+ myuvw = ns.uvw(s1=1,s2=2) << Meq.Subtract(uvw1,uvw2);
 
- RA_root = RJN_sixpack.make_RA(pc_name,RA_0,ns);
- Dec_root = RJN_sixpack.make_Dec(pc_name,Dec_0,ns);
- tworoot = ns[twoname] << Meq.Composer(RA_root,Dec_root);
-
- # 2 stations, yielding 1 baseline / ifr (interferometer)
- stations = range(1,12,10);
- ifrs = TDL_Cohset.stations2ifrs(stations);
- rr = MG_JEN_forest_state.MS_interface_nodes(ns,ra0=RA_0,dec0=Dec_0);
- 
- child_rec = [twoname];
+ # Define the factor of padding zeros to be added
+ padfactor = 1.0;
 
  lmax = 0
  mmax = 0
@@ -172,8 +213,8 @@ def _define_forest(ns):
  # Add one non-zero source (q=testsource) to the Patch
  # I=Q=U=V=1.0
  sname = 'testsource';
- source_RA = RA_0 + 0.0*0.000184026310335*2.99792458e8/1350.0e6;
- source_Dec = Dec_0 + 5.0*0.000183707912445*2.99792458e8/1350.0e6;
+ source_RA = RA_0 + 10.0*0.00018175153567*2.99792458e8/1350.0e6;
+ source_Dec = Dec_0 + 6.0*0.000180822964219*2.99792458e8/1350.0e6;
  sisif = 1.0;
  qin = 1.0;
  uin = 1.0;
@@ -190,9 +231,12 @@ def _define_forest(ns):
  # At this point the Phase Center and all sources are combined
  # into a "source list": child_rec.
 
+ # Define the padding factor
+ padfactor = 1.0;
+
  # Create the SixPack for the Patch Image
  # PatchComposer (SixPack)
- patch_root = ns['Patch['+pc_name+']'] << Meq.PatchComposer(children=child_rec,Method=1);
+ patch_root = ns['Patch['+pc_name+']'] << Meq.PatchComposer(children=child_rec,UVppw=padfactor);
 
  # Select the 4 Stokes planes
  # Selector (FourPack)
@@ -200,24 +244,13 @@ def _define_forest(ns):
 
  # Go from 4 Stokes planes to 4 Correlation planes
  # Stokes
- stokes_root = ns['Stokes['+pc_name+']'] << Meq.Stokes(children=select_root);
+ corr_root = ns['Corr['+pc_name+']'] << Meq.Stokes(children=select_root);
 
  # FFT the 4 correlations
  # FFTBrick
  # NB: since UVInterpol expects LM axes instead of UV (why????),
  # temporarily tell the FFTBrick to do just that
- fft_root = ns['FFT['+pc_name+']'] << \
-    Meq.FFTBrick(children=stokes_root,axes_out=(dmi.hiid("l"),dmi.hiid("m")));
-
- for ifr in ifrs:
- # for every baseline
- #
- # create uvw coordinates
-  skey1 = TDL_radio_conventions.station_key(ifr[0]);
-  skey2 = TDL_radio_conventions.station_key(ifr[1]);
-  uvw1 = rr.uvw[skey1];
-  uvw2 = rr.uvw[skey2];
-  uvw_root = ns.uvw(s1=skey1,s2=skey2) << Meq.Subtract(uvw1,uvw2);
+ fft_root = ns['FFT['+pc_name+']'] << Meq.FFTBrick(children=corr_root,uvppw=padfactor);
  
  # The actual Interpolation
  #  Method = 1: Bi-Cubic Hermite Interpolation (most accurate)
@@ -228,32 +261,32 @@ def _define_forest(ns):
  #    are plotted on the UV plane
  #
  #UVInterpol
- interpol_root = ns['UVInterpol['+pc_name+']']<<Meq.UVInterpol(Method=1,children=[fft_root,uvw_root],additional_info=True);
+ interpol_root = ns['UVInterpol['+pc_name+']']<<Meq.UVInterpol(Method=1,brick=fft_root, uvw=myuvw,additional_info=True);
  
  # Now create the DFT node, to which we will compare the UVInterpol result.
  #
  #RA,Dec,RA_0,Dec_0
- ra_array = array([source_RA/0.001]);
- dec_array=array([source_Dec/0.001]);
+ ra_array = array([source_RA/0.0001]);
+ dec_array=array([source_Dec/0.0001]);
  ra_polc=meq.polc(coeff=ra_array);
  dec_polc=meq.polc(coeff=dec_array);
  nRA_root = ns['nRA']<<Meq.Parm(ra_polc,node_groups='Parm');
  nDec_root = ns['nDec']<<Meq.Parm(dec_polc,node_groups='Parm');
- dRA_root = ns['dRA']<<Meq.Constant(0.001);
- dDec_root = ns['dDec']<<Meq.Constant(0.001);
+ dRA_root = ns['dRA']<<Meq.Constant(0.0001);
+ dDec_root = ns['dDec']<<Meq.Constant(0.0001);
  RA_root = ns['RA']<<Meq.Multiply(children=[nRA_root,dRA_root]);
  Dec_root = ns['Dec']<<Meq.Multiply(children=[nDec_root,dDec_root]);
 
  radec1 = ns.radec1(q='dft') << Meq.Composer(RA_root,Dec_root);
- lmn = ns.lmn(q='dft') << Meq.LMN(radec_0=tworoot,radec=radec1);
- #n = ns.n(q='dft') << Meq.Constant(1.0);
- n = ns.n(q='dft') << Meq.Selector(children=lmn,index=2);
+ lmn = ns.lmn(q='dft') << Meq.LMN(radec_0=radec0,radec=radec1);
+ n = ns.n(q='dft') << Meq.Constant(1.0);
+ #n = ns.n(q='dft') << Meq.Selector(children=lmn,index=2);
  lmn1 = ns.lmn_minus1(q='dft') << Meq.Paster(lmn,n-1,index=2);
  sqrtn = ns << Meq.Sqrt(n);
 
  # DFT
  #dft_root = ns['DFT']<<Meq.Multiply(children=[Meq.Constant(-1.0),Meq.VisPhaseShift(lmn=lmn_root,uvw=uvw_root)]);
- dft_root = ns['DFT']<<Meq.VisPhaseShift(lmn=lmn1,uvw=uvw_root);
+ dft_root = ns['DFT']<<Meq.VisPhaseShift(lmn=lmn1,uvw=myuvw);
 
  # Compare the XX result between UVInterpol and DFT
  #
@@ -275,12 +308,12 @@ def _define_forest(ns):
  solver_root = ns['Solver']<<Meq.Solver(children=[cond_root],num_iter=100,debug_level=20,solvable=solvables);
 
  # Define Bookmarks
- MG_JEN_forest_state.bookmark(cond_root,page="CondEq",viewer="Result Plotter");
- MG_JEN_forest_state.bookmark(select2_root,page="FT-Result",viewer="Result Plotter");
- MG_JEN_forest_state.bookmark(dft_root,page="FT-Result",viewer="Result Plotter");
- MG_JEN_forest_state.bookmark(fft_root,page="UVBrick",viewer="Result Plotter");
- MG_JEN_forest_state.bookmark(interpol_root,udi="cache/result/uvinterpol_map",page="UVBrick",viewer="Result Plotter");
- MG_JEN_forest_state.bookmark(patch_root,page="Patch Image",viewer="Result Plotter");
+ JEN_bookmarks.create(cond_root,page="CondEq",viewer="Result Plotter");
+ JEN_bookmarks.create(select2_root,page="FT-Result",viewer="Result Plotter");
+ JEN_bookmarks.create(dft_root,page="FT-Result",viewer="Result Plotter");
+ JEN_bookmarks.create(fft_root,page="UVBrick",viewer="Result Plotter");
+ JEN_bookmarks.create(interpol_root,udi="cache/result/uvinterpol_map",page="UVBrick",viewer="Result Plotter");
+ JEN_bookmarks.create(patch_root,page="Patch Image",viewer="Result Plotter");
 
 ########################################################################
 
