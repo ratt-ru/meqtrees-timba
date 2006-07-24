daf5c72a8314702cbed25d7bef1adadbfc4208e1
Ronald Nijboer <rnijboer@astron.nl>
2006-07-24 13:38:30 +0000

    Updated version

--- WH/contrib/RJN/MG_RJN_UVBrick_FITS.py
+++ WH/contrib/RJN/MG_RJN_UVBrick_FITS.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python
 
+
+#% $Id$ 
+
+#
+# Copyright (C) 2006
+# ASTRON (Netherlands Foundation for Research in Astronomy)
+# and The MeqTree Foundation
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
 script_name = 'MG_RJN_UVBrick_FITS.py'
 
 # Short description:
@@ -40,8 +64,10 @@ def _define_forest(ns):
     
  # Construction of a Phase Center Two-Pack (RA, DEC)
  # RA and Dec coordinates of the Patch Phase Center
- RA_0 = 4.35664870004
- Dec_0 = 1.09220644132
+ #RA_0 = 4.35664870004
+ #Dec_0 = 1.09220644132
+ RA_0 = 1.45965879284;
+ Dec_0 = 0.384111972073;
  ra0 = ns.ra0 << RA_0;
  dec0 = ns.dec0 << Dec_0;
  radec0 = ns.radec0 << Meq.Composer(ra0,dec0);
@@ -59,7 +85,7 @@ def _define_forest(ns):
  xyz0 = ns.xyz0 << Meq.Composer(x0,y0,z0);
  # Station 1
  X1 = 0.0;
- Y1 = 900.0;
+ Y1 = 2700.0;
  Z1 = 0.0;
  x1 = ns.x(s1=1) << X1;
  y1 = ns.y(s1=1) << Y1;
@@ -79,20 +105,18 @@ def _define_forest(ns):
  myuvw = ns.uvw(s1=1,s2=2) << Meq.Subtract(uvw1,uvw2);
 
  # Define the factor of padding zeros to be added
- padfactor = 30.0;
+ padfactor = 3.0;
 
  # Create the SixPack for the FITS Image
  # Image (Sixpack)
  home_dir = os.environ['HOME']
- infile_name = home_dir + '/LOFAR/Timba/WH/contrib/RJN/source1a.fits'
- image_root = ns.image << Meq.FITSImage(filename=infile_name,cutoff=1.0);
+ infile_name = home_dir + '/LOFAR/Timba/WH/contrib/RJN/3.fits'
+ image_root = ns.image << Meq.FITSImage(filename=infile_name,cutoff=0.8);
 
  # Select the 4 Stokes planes
  #Selector (FourPack)
  iquv_root = ns.IQUV << Meq.Selector(children=image_root,multi=True,index=[2,3,4,5]);
- # One could also resample here (when the resampler works correctly)
- #resampler2 = ns.resampler2 << Meq.Resampler(iquv_root,common_axes=[hiid('l'),hiid('m')]);
-
+ 
  # Go from 4 Stokes planes to 4 Correlation planes
  #Stokes
  corr_root = ns.corr << Meq.Stokes(children=iquv_root);
@@ -119,18 +143,19 @@ def _define_forest(ns):
  JEN_bookmarks.create(interpol_root,udi="cache/result/uvinterpol_map",page="UVBrick",viewer="Result Plotter");
  JEN_bookmarks.create(image_root,page="Image",viewer="Result Plotter");
  JEN_bookmarks.create(interpol_root,page="Freq Time Result",viewer="Result Plotter");
+ JEN_bookmarks.create(resampler_root,page="Freq Time Result",viewer="Result Plotter");
 
 ########################################################################
 
 def _test_forest(mqs,parent):
 
  # Create the Request Cells
- f0 = 1350.0e6
- f1 = 1450.0e6
+ f0 = 100.0e6
+ f1 = 200.0e6
  t0 = 0.0
  t1 = 86400.0
  nfreq = 3
- ntime = 100
+ ntime = 1000
  
  # create cell
  freqtime_domain = meq.domain(startfreq=f0, endfreq=f1, starttime=t0, endtime=t1);
