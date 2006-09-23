8f88dc474d7f377be208d929763bee6f9c2ecf0d
Tony Willis <twillis@drao.nrc.ca>
2006-09-23 04:45:29 +0000

    Added intensity color bar and camera placement and zoom now seem to be well proportioned.

--- PyApps/src/Plugins/vtk_image_warp.py
+++ PyApps/src/Plugins/vtk_image_warp.py
@@ -30,6 +30,9 @@ for k in range(num_arrays):
     else:
       image_numarray[0,k,i] =  sin(dist) / dist
 
+data_min = image_numarray.min()
+data_max = image_numarray.max()
+
 # now display it
 image_array = vtkImageImportFromNumarray()
 image_array.SetArray(image_numarray)
@@ -38,27 +41,32 @@ geometry = vtk.vtkImageDataGeometryFilter()
 geometry.SetInput(image_array.GetOutput())
 warp = vtk.vtkWarpScalar()
 warp.SetInput(geometry.GetOutput())
-warp.SetScaleFactor(50.0)
+scale_factor = 50.0
+warp.SetScaleFactor(scale_factor)
 
-# Use vtkMergeFilter to combine the original image with the warped
-# geometry.
-#mapper = vtk.vtkDataSetMapper()
+# create blue to red color table
+lut = vtk.vtkLookupTable()
+lut.SetHueRange(0.6667, 0.0)
+lut.SetNumberOfColors(256)
+lut.Build()
+lut.SetRange(scale_factor*data_min, scale_factor*data_max)
+
 mapper = vtk.vtkPolyDataMapper();
 mapper.SetInput(warp.GetPolyDataOutput())
-mapper.SetScalarRange(0, 255)
+mapper.SetLookupTable(lut)
 mapper.ImmediateModeRenderingOff()
 actor = vtk.vtkActor()
 actor.SetMapper(mapper)
 
-#  Make the bounding box
-outline = vtk.vtkOutlineSource();
-xMin, xMax, yMin, yMax, zMin, zMax = image_array.GetDataExtent()
-outline.SetBounds(xMin, xMax, yMin, yMax, 0, 50)
-outline_mapper = vtk.vtkPolyDataMapper();
-outline_mapper.SetInput(outline.GetOutput() );
-outline_actor = vtk.vtkActor();
-outline_actor.SetMapper(outline_mapper);
-outline_actor.VisibilityOn();
+# create scalar bar
+scalar_bar = vtk.vtkScalarBarActor()
+scalar_bar.SetLookupTable(lut)
+scalar_bar.SetOrientationToVertical()
+scalar_bar.SetWidth(0.1)
+scalar_bar.SetHeight(0.8)
+scalar_bar.SetTitle("Intensity")
+scalar_bar.GetPositionCoordinate().SetCoordinateSystemToNormalizedViewport()
+scalar_bar.GetPositionCoordinate().SetValue(0.01, 0.1)
 
 # Create renderer stuff
 ren = vtk.vtkRenderer()
@@ -71,6 +79,16 @@ iren.Initialize()
 camstyle = vtk.vtkInteractorStyleTrackballCamera()
 iren.SetInteractorStyle(camstyle)
 
+#  Make the bounding box
+outline = vtk.vtkOutlineSource();
+xMin, xMax, yMin, yMax, zMin, zMax = image_array.GetDataExtent()
+outline.SetBounds(xMin, xMax, yMin, yMax, data_min * scale_factor, data_max* scale_factor)
+outline_mapper = vtk.vtkPolyDataMapper();
+outline_mapper.SetInput(outline.GetOutput() );
+outline_actor = vtk.vtkActor();
+outline_actor.SetMapper(outline_mapper);
+outline_actor.VisibilityOn();
+
 # Create a text property for cube axes
 tprop = vtk.vtkTextProperty()
 tprop.SetColor(1, 1, 1)
@@ -79,11 +97,10 @@ tprop.ShadowOn()
 # Create a vtkCubeAxesActor2D.  Use the outer edges of the bounding box to
 # draw the axes.  Add the actor to the renderer.
 axes = vtk.vtkCubeAxesActor2D()
-xMin, xMax, yMin, yMax, zMin, zMax = image_array.GetDataExtent()
-axes.SetBounds(xMin, xMax, yMin, yMax, 0, 50 )
+axes.SetBounds(xMin, xMax, yMin, yMax,  data_min * scale_factor, data_max* scale_factor)
 axes.SetCamera(ren.GetActiveCamera())
 axes.SetLabelFormat("%6.4g")
-#axes.SetFlyModeToOuterEdges()
+axes.SetFlyModeToOuterEdges()
 axes.SetFontFactor(0.8)
 axes.SetAxisTitleTextProperty(tprop)
 axes.SetAxisLabelTextProperty(tprop)
@@ -95,7 +112,7 @@ ren.AddProp(axes)
 # Add the actors to the renderer, set the background and size
 ren.AddActor(actor)
 ren.AddActor(outline_actor);
-#ren.GetActiveCamera().SetViewUp(0,0,1)
+ren.AddActor2D(scalar_bar)
 ren.SetBackground(0.1, 0.2, 0.4)
 ren.ResetCameraClippingRange()
 
@@ -107,7 +124,7 @@ cy = 0.5*(yMax-yMin)
 cz = 0
 cam1.SetFocalPoint(cx, cy, cz)
 cam1.SetPosition(cx,cy, cz)
-cam1.Azimuth(20)
+cam1.Azimuth(40)
 cam1.Elevation(30)
 cam1.Zoom(-5)
 ren.ResetCamera()
