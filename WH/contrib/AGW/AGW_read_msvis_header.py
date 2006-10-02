28d73df476edf09b047657c37b9e984347f7a108
Tony Willis <twillis@drao.nrc.ca>
2006-10-02 21:20:50 +0000

    Added MeqTrees copyright

--- WH/contrib/AGW/AGW_read_msvis_header.py
+++ WH/contrib/AGW/AGW_read_msvis_header.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python
 
+#% $Id$ 
+
+#
+# Copyright (C) 2006
+# ASTRON (Netherlands Foundation for Research in Astronomy)
+# and The MeqTree Foundation
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
 # Allows the kernel to update various nodes in the tree
 # By reading in stuff from the measurement set header.
 # Just taken from the MAB_read_msvis_header.py script in
 # the LOFAR/Timba/MeqServer/test directory
+
 
 from Timba.meqkernel import set_state
 
