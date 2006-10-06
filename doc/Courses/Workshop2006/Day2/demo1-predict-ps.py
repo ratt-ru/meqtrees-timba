de399cd355189bf8312137e2bd7f5e4d368a6c83
Oleg Smirnov <osmirnov@gmail.com>
2006-10-06 13:51:40 +0000

    Added imaging script

--- doc/Courses/Workshop2006/Day2/demo1-predict-ps.py
+++ doc/Courses/Workshop2006/Day2/demo1-predict-ps.py
@@ -4,67 +4,82 @@ from Timba.Meq import meq
 import math
 
 # define antenna list
-ANTENNAS = range(27);
+ANTENNAS = range(1,28);
 # derive interferometer list
 IFRS   = [ (p,q) for p in ANTENNAS for q in ANTENNAS if p<q ];
 
 # useful constant: 1 deg in radians
-DEG = 2*math.pi/180.;
+DEG = math.pi/180.;
 
 # source parameters
 I = 1; Q = .2; U = .2; V = .2;
-L = 2*(DEG/60);
-M = 2*(DEG/60);
+L = 1*(DEG/60);
+M = 1*(DEG/60);
 N = math.sqrt(1-L*L-M*M);
 
 
 def _define_forest (ns):
   # nodes for phase center
-  ns.radec0 = Meq.Composer(ns.ra0<<Meq.Parm,ns.dec0<<Meq.Parm);
+  ns.radec0 = Meq.Composer(ns.ra0<<0,ns.dec0<<0);
   
-  # nodes for array and antenna positions
-  ns.xyz0 = Meq.Composer(ns.x0<<Meq.Parm,ns.y0<<Meq.Parm,ns.z0<<Meq.Parm);
-  # define per-station stuff
+  # nodes for array reference position
+  ns.xyz0 = Meq.Composer(ns.x0<<0,ns.y0<<0,ns.z0<<0);
+  
+  # now define per-station stuff: XYZs and UVWs 
   for p in ANTENNAS:
     # positions and uvw
-    ns.xyz(p) << Meq.Composer(ns.x(p) << Meq.Parm,
-                              ns.y(p) << Meq.Parm,
-                              ns.z(p) << Meq.Parm);
+    ns.xyz(p) << Meq.Composer(ns.x(p)<<0,ns.y(p)<<0,ns.z(p)<<0);
     ns.uvw(p) << Meq.UVW(radec=ns.radec0,xyz_0=ns.xyz0,xyz=ns.xyz(p));
   
-  # source LMN
+  # source l,m,n-1 vector
   ns.lmn_minus1 << Meq.Composer(L,M,N-1);
   
   # define K-jones matrices
   for p in ANTENNAS:
     ns.K(p) << Meq.VisPhaseShift(lmn=ns.lmn_minus1,uvw=ns.uvw(p));
-    ns.conjK(p) << Meq.ConjTranspose(ns.K(p));
+    ns.Kt(p) << Meq.ConjTranspose(ns.K(p));
   
   # define source brightness, B
-  ns.B << Meq.Matrix22(I+Q,Meq.ToComplex(U,-V),Meq.ToComplex(U,V),I-Q);
-  # and B/n
-  ns.B_n = ns.B/N;
+  ns.B << 0.5 * Meq.Matrix22(I+Q,Meq.ToComplex(U,-V),Meq.ToComplex(U,V),I-Q);
   
-  # define predicted visibilities, attach to sinks
+  # now define predicted visibilities, attach to sinks
   for p,q in IFRS:
     predict = ns.predict(p,q) << \
-      Meq.MatrixMultiply(ns.K(p),ns.B_n,ns.conjK(q));
-    ns.sink(p,q) << Meq.Sink(predict,station_1_index=p,station_2_index=q,output_col='DATA');
+      Meq.MatrixMultiply(ns.K(p),ns.B,ns.Kt(q));
+    ns.sink(p,q) << Meq.Sink(predict,station_1_index=p-1,station_2_index=q-1,output_col='DATA');
 
   # define VisDataMux
   ns.vdm << Meq.VisDataMux(*[ns.sink(p,q) for p,q in IFRS]);
   
-  
+
+def _test_forest (mqs,parent):
+  # create an I/O request
+  req = meq.request();
+  req.input = record( 
+    ms = record(  
+      ms_name          = 'demo.MS',
+      data_column_name = 'DATA',
+      tile_size        = 30
+    ),
+    python_init = 'Timba.Contrib.OMS.ReadVisHeader'
+  );
+  req.output = record( 
+    ms = record( 
+      data_column = 'DATA' 
+    )
+  );
+  # execute    
+  mqs.execute('vdm',req,wait=False);
 
 
-
-def _test_forest (mqs):
-  """test_forest() is a standard TDL name. When a forest script is
-  loaded by, e.g., the browser, and the "test" option is set to true,
-  this method is automatically called after define_forest() to run a 
-  test on the forest. The 'mqs' argument is a meqserver proxy object.
-  """;
-  from Timba.Meq import meq
+# setup a few bookmarks
+Settings.forest_state = record(bookmarks=[
+  record(name='K Jones',page=[
+    record(udi="/node/K:1",viewer="Result Plotter",pos=(0,0)),
+    record(udi="/node/K:2",viewer="Result Plotter",pos=(0,1)),
+    record(udi="/node/K:9",viewer="Result Plotter",pos=(1,0)),
+    record(udi="/node/K:26",viewer="Result Plotter",pos=(1,1)) \
+  ])]);
 
 
 
