09062eeff9216a84682e7c37b57cb80e88cd5f2a
Oleg Smirnov <osmirnov@gmail.com>
2006-11-28 14:52:52 +0000

    Added TDLMenu to globally visible symbols

--- PyApps/src/TDL/__init__.py
+++ PyApps/src/TDL/__init__.py
@@ -4,7 +4,7 @@ from Timba.TDL import TDLimpl
 from Timba.TDL.TDLimpl import *
 from Timba.TDL.MeqClasses import Meq
 from Timba.TDL import Settings
-from Timba.TDL.TDLOptions import TDLOption,TDLCompileOption,TDLRuntimeOption,TDLCompileMenu,TDLRuntimeMenu
+from Timba.TDL.TDLOptions import TDLOption,TDLCompileOption,TDLRuntimeOption,TDLCompileMenu,TDLRuntimeMenu,TDLMenu
 
 _dbg = TDLimpl._dbg;
 _dprint = _dbg.dprint;
