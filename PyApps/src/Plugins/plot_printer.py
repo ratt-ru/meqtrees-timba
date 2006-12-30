a90d20b28c610bd8581eb4a3e1c210cf7382c274
Tony Willis <twillis@drao.nrc.ca>
2006-12-30 01:24:53 +0000

    split off printfilter as a separate script

--- PyApps/src/Plugins/plot_printer.py
+++ PyApps/src/Plugins/plot_printer.py
@@ -29,37 +29,7 @@ try:
   from Qwt4 import *
 except:
   from qwt import *
-
-class PrintFilter(QwtPlotPrintFilter):
-    def __init__(self):
-        QwtPlotPrintFilter.__init__(self)
-
-    # __init___()
-    
-    def color(self, c, item, i):
-        if not (self.options() & QwtPlotPrintFilter.PrintCanvasBackground):
-            if item == QwtPlotPrintFilter.MajorGrid:
-                return Qt.darkGray
-            elif item == QwtPlotPrintFilter.MinorGrid:
-                return Qt.gray
-        if item == QwtPlotPrintFilter.Title:
-            return Qt.red
-        elif item == QwtPlotPrintFilter.AxisScale:
-            return Qt.green
-        elif item == QwtPlotPrintFilter.AxisTitle:
-            return Qt.blue
-        return c
-
-    # color()
-
-    def font(self, f, item, i):
-        result = QFont(f)
-        result.setPointSize(int(f.pointSize()*1.25))
-        return result
-
-    # font()
-
-# class PrintFilter
+import printfilter
 
 class plot_printer:
   def __init__(self, plotter, colorbar=None):
@@ -139,7 +109,7 @@ class plot_printer:
     qprinter.setOutputToFile(True)
     qprinter.setOutputFileName('image_plot.ps')
     if qprinter.setup():
-        filter = PrintFilter()
+        filter = printfilter.PrintFilter()
         if (QPrinter.GrayScale == qprinter.colorMode()):
             filter.setOptions(QwtPlotPrintFilter.PrintAll
                   & ~QwtPlotPrintFilter.PrintCanvasBackground)
