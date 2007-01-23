956822a89e29e434e1253a6667a22a50638001e5
Sarod Yatawatta <yatawatta@astron.nl>
2007-01-23 16:54:39 +0000

    removed cut-paste error (extra Fail)

--- MeqNodes/src/MaxLocation.cc
+++ MeqNodes/src/MaxLocation.cc
@@ -58,7 +58,6 @@ void MaxLocation::computeResultCells (Cells::Ref &ref,const std::vector<Result::
   //remember cells
   const Result &chres = *( childres.front() );
   cells_ <<= new Cells(chres.cells());
-  FailWhen(!cells.isDefined(Axis::TIME),"Meq::MaxLocation: no time axis in request, can't compute AzEls");
   ref.attach(cells);
 }
 
