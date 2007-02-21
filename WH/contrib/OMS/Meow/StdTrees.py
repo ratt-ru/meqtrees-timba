f3d3b8b7583d88ae8dd25705860a78fed20a435e
Oleg Smirnov <osmirnov@gmail.com>
2007-02-21 16:44:26 +0000

    Revised the SolveTree to implement an explicit "output" branch for corrections and such

--- WH/contrib/OMS/Meow/StdTrees.py
+++ WH/contrib/OMS/Meow/StdTrees.py
@@ -22,42 +22,55 @@ class _BaseTree (object):
     
   def inputs (self):
     return self._inputs;
-
-class SolveTree (_BaseTree):
-  def __init__ (self,ns,predict,array=None,observation=None,residuals=None):
+    
+class ResidualTree (_BaseTree):
+  def __init__ (self,ns,predict,array=None,observation=None):
     _BaseTree.__init__(self,ns,array,observation);
     self._predict = predict;
-    self._make_residuals = residuals;
     
-  def outputs (self,inputs=None):
+  def residuals (self,inputs=None):
+    """Makes residual trees, by subtracting predict(p,q) from inputs(p,q).
+    Returns residual node, which must be qualified with a (p,q) pair.
+    """;
     # figure out our inputs
     self.set_inputs(inputs);
-    # select output node type
+    outputs = self.ns.residual;
+    if not outputs(*self.array.ifrs()[0]).initialized():
+      for p,q in self.array.ifrs():
+        outputs(p,q) << self._inputs(p,q) - self._predict(p,q);
+    return outputs;
+
+class SolveTree (ResidualTree):
+  def __init__ (self,ns,predict,array=None,observation=None,residuals=None):
+    ResidualTree.__init__(self,ns,predict,array,observation);
+    self._make_residuals = residuals;
+
+  def outputs (self,inputs=None):
+    """Makes solver tree, returns the set of reqseq output nodes.
+    This is for compatibility with older scripts. The output nodes will be
+    reqseqs returning either inputs or residuals, as chosen in the 
+    constructor.
+    """;
+    self.set_inputs(inputs);
     if self._make_residuals:
-      outputs = self.ns.residual;
+      outputs = self.residuals(inputs);
     else:
       outputs = self._inputs;
-    # now initialize our tree
-    reqseq = self._outputs = self.ns.reqseq;
-    if not reqseq(*self.array.ifrs()[0]).initialized():
-      # create solver node ahead of time
-      solver = self.solver() << Meq.Solver();
+    return self.sequencers(inputs,outputs=outputs);
+    
+  def solver (self,inputs=None):
+    """Makes solver tree given the inputs and the predicts, returns
+    solver node.
+    """;
+    self.set_inputs(inputs);
+    solver = self.ns.solver;
+    if not solver.initialized():
       self._solver_name = solver.name;
       # create condeqs and request sequencers
       for p,q in self.array.ifrs():
         inp  = self._inputs(p,q);
         pred = self._predict(p,q);
         self.ns.ce(p,q) << Meq.Condeq(inp,pred);
-        # output is either spigot itself, or uncorrected residuals.
-        # in the latter case, we also have to define them
-        if outputs is not self._inputs:
-          outp = outputs(p,q) << inp - pred;
-        else:
-          outp = inp;
-        # attach output to request sequencer
-        reqseq(p,q) << Meq.ReqSeq(solver,outp,result_index=1);
-        # adjust number of parents for node cache
-        pred.set_options(cache_num_active_parents=1);
       # create optimal poll order for condeqs, for efficient parallelization
       # (i.e. poll child 1:2, 3:4, 5:6, ..., 13:14,
       # then the rest)
@@ -66,17 +79,37 @@ class SolveTree (_BaseTree):
         (p,q) = self.array.stations()[i*2:(i+1)*2];
         cpo.append(self.ns.ce(p,q).name);
       # add condeqs to solver
-      solver.set_options(child_poll_order=cpo);
-      solver.add_children(*[self.ns.ce(p,q) for p,q in self.array.ifrs()]);
+      solver << Meq.Solver(children=[self.ns.ce(p,q) for p,q in self.array.ifrs()],
+                           child_poll_order=cpo);
+    return solver;
+ 
+  def sequencers (self,inputs=None,outputs=None):
+    """Makes solver tree and adds sequencers to execute an
+    'outputs' branch after the solve is complete.
+    """;
+    # figure out our inputs
+    self.set_inputs(inputs);
+    if outputs is None:
+      raise TypeError,"mandatory 'outputs' argument missing";
       
-    # our output node is the reqseq
+    reqseq = self._outputs = self.ns.reqseq;
+    if not reqseq(*self.array.ifrs()[0]).initialized():
+      solver = self.solver();
+      # create condeqs and request sequencers
+      for p,q in self.array.ifrs():
+        reqseq(p,q) << Meq.ReqSeq(solver,outputs(p,q),result_index=1);
+        outputs(p,q).set_options(cache_num_active_parents=1);
+        
     return reqseq;
     
-  def solver (self):
-    """Returns solver node for this tree""";
-    return self.ns.solver;
-      
   def define_solve_job (self,jobname,jobid,solvables,tile_sizes=[1,10,100],vdm=None):
+    """Defines a "solve job", with a runtime menu entry and its own set of options
+    'jobname':    a descriptive name (will appear in the menu)
+    'jobid':      unique identifier string
+    'solvables':  list of solvables (Parm nodes or node names)
+    'tile_sizes': list of suggested tilings
+    'vdm':        VisDataMux node which runs things, Context.vdm is used if None   
+    """;
     # make sure solvables is a list of names
     def namify (arg):
       if isinstance(arg,str):
