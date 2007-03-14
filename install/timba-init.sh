bc7b63ad13c2117b3236e094b91e7464118c3d7f
Oleg Smirnov <osmirnov@gmail.com>
2007-03-14 13:36:11 +0000

    Added a sh init-script

---
+++ install/timba-init.sh
@@ -0,0 +1,26 @@
+# timba-init.sh
+# source this file into your .bashrc to have all Timba paths set up at login
+
+
+if [ "$TIMBA_PATH" == "" ]; then
+  export TIMBA_PATH=$HOME/Timba
+fi
+
+if [ ! -d $TIMBA_PATH/install ]; then
+  echo "Warning: cannot find Timba install under $TIMBA_PATH"
+  echo "If Timba is installed elsewhere, please set your TIMBA_PATH variable appropriately."
+else
+  export DEFAULT_PATH=$PATH
+  export DEFAULT_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+  export DEFAULT_PYTHON_PATH=$PYTHON_PATH
+
+  _timba-setup()
+  {
+    export PATH=$DEFAULT_PATH:$TIMBA_PATH/install/$1/bin
+    export PYTHONPATH=.:$TIMBA_PATH/install/$1/libexec/python
+    export LD_LIBRARY_PATH=$DEFAULT_LD_LIBRARY_PATH:$TIMBA_PATH/install/$1/lib
+    echo "Using Timba install $TIMBA_PATH/install/$1"
+  }
+
+  _timba-setup current
+fi
