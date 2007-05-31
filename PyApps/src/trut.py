ad18ddf4613cc45ad5537aa17402d9d85e9e9d77
Oleg Smirnov <osmirnov@gmail.com>
2007-05-31 12:40:32 +0000

    Cleaned up handling of command-line options for TRUT

--- PyApps/src/trut.py
+++ PyApps/src/trut.py
@@ -10,15 +10,15 @@ if __name__ == '__main__':
   # strip off debug level options
   args = [ arg for arg in sys.argv[1:] if not arg.startswith('-d') ];
   # parse other options
-  opts,files = getopt.getopt(args,"v:p:l:j:");
+  opts,files = getopt.getopt(args,"v:p:l:j:h",["help"]);
   opts = dict(opts);
   
-  if not files:
-    print "Usage: trut.py [-v<verbosity level>] [-p<persistence level>] [-l<loglevel] [-j<maxjobs>] trutfiles ...."
+  if "-h" in opts or "--help" in opts or not files:
+    print "Usage: trut [-v<verbosity level>] [-p<persistence level>] [-l<loglevel] [-j<maxjobs>] trutfiles ...."
     sys.exit(1);
   
   verbose = int(opts.get('-v',21));
-  log     = int(opts.get('-l',40));
+  log     = int(opts.get('-l',41));
   persist = int(opts.get('-p',0));
   maxjobs = int(opts.get('-j',0));
   
