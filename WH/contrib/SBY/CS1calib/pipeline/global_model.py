643065afb67e7699239665083f17d727690e4570
Sarod Yatawatta <yatawatta@astron.nl>
2007-06-13 17:28:49 +0000

    modified pipeline, added station beam

--- WH/contrib/SBY/CS1calib/pipeline/global_model.py
+++ WH/contrib/SBY/CS1calib/pipeline/global_model.py
@@ -133,7 +133,7 @@ def point_and_extended_sources_solvable(ns,lsm,tablename=''):
      if f0==0:
        f0=150e6
      if eX!=0 or eY!=0 or eP!=0:
-         source_model.append( GaussianSource(ns,name=pu.name,I=Meow.Parm(sI), Q=sQ, U=sU, V=sV,
+         source_model.append( GaussianSource(ns,name=pu.name,I=Meow.Parm(sI), Q=Meow.Parm(sQ), U=Meow.Parm(sU), V=Meow.Parm(sV),
                   Iorder=0, direction=Direction(ns,pu.name,ra,dec,parm_options=parm_options),
                   spi=SIn,freq0=f0,
                   size=[eX,eY],phi=eP,
@@ -141,12 +141,12 @@ def point_and_extended_sources_solvable(ns,lsm,tablename=''):
      else:
        # point sources
        if RM==0: 
-         source_model.append( PointSource(ns,name=pu.name,I=Meow.Parm(sI), Q=sQ, U=sU, V=sV,
+         source_model.append( PointSource(ns,name=pu.name,I=Meow.Parm(sI), Q=Meow.Parm(sQ), U=Meow.Parm(sU), V=Meow.Parm(sV),
                   direction=Direction(ns,pu.name,ra,dec),
                   spi=SIn,freq0=f0));
 
        else:
-         source_model.append( PointSource(ns,name=pu.name,I=Meow.Parm(sI), Q=sQ, U=sU, V=sV,
+         source_model.append( PointSource(ns,name=pu.name,I=Meow.Parm(sI), Q=Meow.Parm(sQ), U=Meow.Parm(sU), V=Meow.Parm(sV),
                   direction=Direction(ns,pu.name,ra,dec,parm_options=parm_options),
                   spi=SIn,freq0=f0, RM=RM,
                   parm_options=parm_options));
@@ -569,6 +569,69 @@ def EJones_droopy_comp(ns,array,sources,radec0,meptable=None,solvables=[],solvab
 
   return Ej0;
 
+
+#### even more complex droopy dipole - with station beam
+def EJones_droopy_comp_stat(ns,array,sources,radec0,meptable=None,solvables=[],solvable=False, name="E"):
+  Bx_phi={}
+  Bx_theta={}
+  By_phi={}
+  By_theta={}
+  
+  for station in array.stations():
+   Bx_phi[station] = makebeam_droopy_phi(ns,station=station,meptable=meptable,solvable=solvable,solvables=solvables);
+   Bx_theta[station] = makebeam_droopy_theta(ns,station=station,meptable=meptable,solvable=solvable,solvables=solvables);
+   By_phi[station] = makebeam_droopy_phi(ns,pol='Y',station=station,meptable=meptable,solvable=solvable,solvables=solvables);
+   By_theta[station] = makebeam_droopy_theta(ns,pol='Y',station=station,meptable=meptable,solvable=solvable,solvables=solvables);
+
+  Ej0 = ns[name];
+
+  # get array xyz
+  xyz=array.xyz();
+
+  # station beam
+  Xstatbeam=ns.Xstatbeam<<Meq.StationBeam(filename="AntennaCoords",RA=-0.159697626557,Dec=1.02651539956,x=3826815.56096,y=460986.585898,z=5064718.84237,phi0=-math.pi/4)
+  Ystatbeam=ns.Ystatbeam<<Meq.StationBeam(filename="AntennaCoords",RA=-0.159697626557,Dec=1.02651539956,x=3826815.56096,y=460986.585898,z=5064718.84237,phi0=-math.pi/4)
+
+  # create per-direction, per-station E Jones matrices
+  for src in sources:
+    dirname = src.direction.name;
+    radec=src.direction.radec()
+    Ej = Ej0(dirname);
+
+    # create Az,El per source, using station 1
+    azelnode=ns.azel(dirname)<<Meq.AzEl(radec=src.direction.radec(),xyz=xyz(1))
+    # make shifts
+    az=ns.az(dirname)<<Meq.Selector(azelnode,multi=True,index=[0])
+    azX=ns.azX(dirname)<<az-math.pi/4
+    azY=ns.azY(dirname)<<az-math.pi/4
+    el=ns.el(dirname)<<Meq.Selector(azelnode,multi=True,index=[1])
+   
+    # sin,cosines
+    #az_CX=ns.az_CX(dirname)<<Meq.Cos(az);
+    #az_SX=ns.az_SX(dirname)<<Meq.Sin(az);
+    #az_CY=ns.az_CY(dirname)<<Meq.Cos(az);
+    #az_SY=ns.az_SY(dirname)<<Meq.Sin(az);
+    #el_S=ns.el_S(dirname)<<Meq.Sin(el);
+
+    for station in array.stations():
+        azelX =ns.azelX(dirname,station)<<Meq.Composer(azX,el)
+        azelY =ns.azelY(dirname,station)<<Meq.Composer(azY,el)
+        Xediag_phi = ns.Xediag_phi(dirname,station) << Meq.Compounder(children=[azelX,Bx_phi[station]],common_axes=[hiid('l'),hiid('m')])
+        Xediag_theta= ns.Xediag_theta(dirname,station) << Meq.Compounder(children=[azelX,Bx_theta[station]],common_axes=[hiid('l'),hiid('m')])
+        Yediag_phi = ns.Yediag_phi(dirname,station) << Meq.Compounder(children=[azelY,By_phi[station]],common_axes=[hiid('l'),hiid('m')])
+        Yediag_theta = ns.Yediag_theta(dirname,station) << Meq.Compounder(children=[azelY,By_theta[station]],common_axes=[hiid('l'),hiid('m')])
+        # create E matrix
+        #Ej(station) <<Meq.Matrix22(el_S*az_CX*Xediag_theta,az_SX*Xediag_phi,Meq.Negate(el_S*az_SY)*Yediag_theta,az_CY*Yediag_phi)
+        if station==1:
+          Xstatgain=ns.Xstatgain(dirname,station)<<Meq.Compounder(children=[azelX,Xstatbeam],common_axes=[hiid('l'),hiid('m')])
+          Ystatgain=ns.Ystatgain(dirname,station)<<Meq.Compounder(children=[azelY,Ystatbeam],common_axes=[hiid('l'),hiid('m')])
+          Ej(station) <<Meq.Matrix22(Xstatgain*Xediag_theta,Xstatgain*Xediag_phi,Ystatgain*Yediag_theta,Ystatgain*Yediag_phi)
+          #Ej(station) <<Meq.Matrix22(Xediag_theta,Xediag_phi,Yediag_theta,Yediag_phi)
+        else:
+          Ej(station) <<Meq.Matrix22(Xediag_theta,Xediag_phi,Yediag_theta,Yediag_phi)
+        #Ej(station) <<Meq.Matrix22(el_S*az_S*Xediag_theta,az_C*Xediag_phi,Meq.Negate(el_S*az_S)*Yediag_theta,az_C*Yediag_phi)
+
+  return Ej0;
 
 
 
--- WH/contrib/SBY/CS1calib/pipeline/runpipeline.py
+++ WH/contrib/SBY/CS1calib/pipeline/runpipeline.py
@@ -19,57 +19,67 @@ import global_model
 import solver_plots
 from Timba.LSM.LSM import LSM
 
+TDLRuntimeMenu("MS Selection",
 ### MS params ####
 # MS names - in a text file, line by line
-TDLRuntimeOption('msnames',"MS Names",["filelist.txt"],inline=True,doc="File List");
+  TDLOption('msnames',"MS Names",["filelist.txt"],inline=True,doc="File List"),
 # read data from
-TDLRuntimeOption('input_column',"Input MS column",["DATA","MODEL_DATA","CORRECTED_DATA"],default=0);
+  TDLOption('input_column',"Input MS column",["DATA","MODEL_DATA","CORRECTED_DATA"],default=0),
 # write data to
-TDLRuntimeOption('output_column',"Output MS column",[None,"MODEL_DATA","CORRECTED_DATA"],default=0);
+  TDLOption('output_column',"Output MS column",[None,"MODEL_DATA","CORRECTED_DATA"],default=0),
 # number of spectral windows to process 
-TDLRuntimeOption('min_spwid',"Start subband",[1,2,3]);
-TDLRuntimeOption('max_spwid',"End subband",[1,2,3,4,5,6]);
+  TDLOption('min_spwid',"Start subband",[1,2,3]),
+  TDLOption('max_spwid',"End subband",[1,2,3,4,5,6]),
 # number of channels per job
-TDLRuntimeOption('channel_step',"channels per job",[8,2,10,20]);
+  TDLOption('channel_step',"channels per job",[8,2,10,20]),
 # start channel in subband (from 0)
-TDLRuntimeOption('start_channel',"start channel",[31,99]);
+  TDLOption('start_channel',"start channel",[31,99]),
 # end channel in subband (from 0)
-TDLRuntimeOption('end_channel',"end channel",[223,41,240]);
+  TDLOption('end_channel',"end channel",[223,41,240])
+);
 
+
+TDLRuntimeMenu("Solver",
+### MS params ####
 #### solver params ####
 # how much to perturb starting values of solvables
-TDLRuntimeOption('perturbation',"Perturb solvables",["random",.1,.2,-.1,-.2]);
+TDLOption('perturbation',"Perturb solvables",["random",.1,.2,-.1,-.2]),
 # solver debug level
-TDLRuntimeOption('solver_debug_level',"Solver debug level",[0,1,10]);
+TDLOption('solver_debug_level',"Solver debug level",[0,1,10]),
 # solver debug level
-TDLRuntimeOption('solver_lm_factor',"Initial solver LM factor",[1,.1,.01,.001]);
+TDLOption('solver_lm_factor',"Initial solver LM factor",[1,.1,.01,.001]),
 # max number of iterations
-TDLRuntimeOption('solver_maxiter',"Max iterations",[10,3,15]);
+TDLOption('solver_maxiter',"Max iterations",[10,3,15]),
 # number of timeslots to use at once
-TDLRuntimeOption('tile_size',"Tile size",[1,10,30,48,60,96,480]);
+TDLOption('tile_size',"Tile size",[1,10,30,48,60,96,480]),
 # solve for full J Jones or  diagonal J Jones
-TDLRuntimeOption('full_J',"Full Jones",[False,True]);
-# which solution used for correction?
-TDLCompileOption('ph_center',"Phase Cen",["CasA","CygA"]);
+TDLOption('full_J',"Full Jones",[False,True]),
+);
 
+
+TDLRuntimeMenu("Imager",
 #### imaging options ####
-TDLRuntimeOption('full_images',"Full Images",[True,False]);
+TDLOption('full_images',"Full Images",[True,False]),
+### uv distance #####
+TDLOption('min_uv',"Min uv distance squared",[3400,400],more=float)
+);
 
 
-
+TDLCompileMenu("Parms",
 # parm table name
-TDLCompileOption('mytable',"parmtable",[None,"peel0.mep"],default=None);
+TDLOption('mytable',"parmtable",[None,"peel0.mep"],default=None),
 # save nonconverged solutions
-TDLCompileOption('mysave',"save all",[False,True],default=False);
+TDLOption('mysave',"save all",[False,True],default=False),
 # subtile solutions
-TDLCompileOption('dosubtile',"subtile",[True,False],default=True);
+TDLOption('dosubtile',"subtile",[True,False],default=True),
 # solve for real/imag or gain/phase
-TDLCompileOption('gain_phase_solution',"Gain/Phase",[True,False],default=False);
+TDLOption('gain_phase_solution',"Gain/Phase",[True,False],default=False),
+# which solution used for correction?
+TDLOption('ph_center',"Phase Cen",["CasA","CygA"])
+);
+
 # correct for all baselines
 TDLCompileOption('include_short_base',"All Baselines",[True,False],default=True);
-
-
-
 # number of stations
 TDLCompileOption('num_stations',"Number of stations",[16,14,15,3]);
 
@@ -474,7 +484,7 @@ def _do_postprocess(fname,mqs):
   if fname==None: return
   # post processing will make images, and calculate mean image - dont wait here 
   # because we will go on to the next MS
-  os.spawnvp(os.P_NOWAIT,'glish',['glish','-l','postprocess.g','args','ms='+fname,'minspwid='+str(min_spwid),'maxspwid='+str(max_spwid),'startch='+str(start_channel+1),'endch='+str(end_channel+1),'step='+str(channel_step)]);
+  os.spawnvp(os.P_NOWAIT,'glish',['glish','-l','postprocess.g','args','ms='+fname,'minspwid='+str(min_spwid),'maxspwid='+str(max_spwid),'startch='+str(start_channel+1),'endch='+str(end_channel+1),'step='+str(channel_step),'minuv='+str(min_uv)]);
   # residual plots
   for spwid in range(min_spwid-1,max_spwid):
     for schan in range(start_channel,end_channel,channel_step):
