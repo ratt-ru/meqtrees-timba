9608ef0954e8dc3b1afb6c82aae32d070e4d297c
Oleg Smirnov <osmirnov@gmail.com>
2007-06-25 12:43:29 +0000

    Made install script smarter w.r.t. guessing the right TIMBA_PATH if not set

--- install/timba-init.sh
+++ install/timba-init.sh
@@ -2,23 +2,42 @@
 # source this file into your .bashrc to have all Timba paths set up at login
 
 
-if [ "$TIMBA_PATH" == "" ]; then
-  export TIMBA_PATH=$HOME/Timba
+if [ -z "$TIMBA_PATH" ]; then
+  # under bash, we can figure out the name of our script, and extract the path
+  if [ "$BASH_SOURCE" != "" ]; then
+    if [ "${BASH_SOURCE[0]/Timba\//}" != "${BASH_SOURCE[0]}" ]; then
+      export TIMBA_PATH="${BASH_SOURCE[0]/Timba\/*/Timba}"
+      # if we end up with a relative path, make it absolute
+      if [ "${TIMBA_PATH#/}" == "$TIMBA_PATH" ]; then
+        export TIMBA_PATH=`pwd`/$TIMBA_PATH
+      fi
+    fi
+  fi
+  if [ -z "$TIMBA_PATH" ]; then
+    export TIMBA_PATH=$HOME/Timba
+  fi
+  echo "Using \$TIMBA_PATH $TIMBA_PATH"
 fi
 
 if [ ! -d $TIMBA_PATH/install ]; then
   echo "Warning: cannot find Timba install under $TIMBA_PATH"
   echo "If Timba is installed elsewhere, please set your TIMBA_PATH variable appropriately."
 else
-  export DEFAULT_PATH=$PATH
-  export DEFAULT_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-  export DEFAULT_PYTHONPATH=$PYTHONPATH
+  if [ "$PRE_TIMBA_PATH" == "" ]; then
+    export PRE_TIMBA_PATH=$PATH
+  fi
+  if [ "$PRE_TIMBA_LD_LIBRARY_PATH" == "" ]; then
+    export PRE_TIMBA_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+  fi
+  if [ "$PRE_TIMBA_PYTHONPATH" == "" ]; then
+    export PRE_TIMBA_PYTHONPATH=$PYTHONPATH
+  fi
 
   _timba-setup()
   {
-    export PATH=$DEFAULT_PATH:$TIMBA_PATH/install/$1/bin
-    export PYTHONPATH=$DEFAULT_PYTHONPATH:.:$TIMBA_PATH/install/$1/libexec/python
-    export LD_LIBRARY_PATH=$DEFAULT_LD_LIBRARY_PATH:$TIMBA_PATH/install/$1/lib
+    export PATH=$PRE_TIMBA_PATH:$TIMBA_PATH/install/$1/bin
+    export PYTHONPATH=$PRE_TIMBA_PYTHONPATH:.:$TIMBA_PATH/install/$1/libexec/python
+    export LD_LIBRARY_PATH=$PRE_TIMBA_LD_LIBRARY_PATH:$TIMBA_PATH/install/$1/lib
     echo "Using Timba install $TIMBA_PATH/install/$1"
   }
 
