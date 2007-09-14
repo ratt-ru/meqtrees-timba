06747b9c0b69ba038bd32e89ef47a0c35b0591a4
Tony Willis <twillis@drao.nrc.ca>
2007-09-14 17:48:06 +0000

    added code to turn sorting of

--- PyApps/src/Plugins/VellsTree.py
+++ PyApps/src/Plugins/VellsTree.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python
+
+#
+# Copyright (C) 2002-2007
+# ASTRON (Netherlands Foundation for Research in Astronomy)
+# and The MeqTree Foundation
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
 import sys
 from qt import *
 
@@ -31,11 +53,16 @@ class VellsView( QListView) :
     self.connect( self, SIGNAL("returnPressed(QListViewItem *)"),
                       self.slotFolderSelected )
 
+    self.addColumn("VellsSelector")
+    self.setSorting(-1)
+    self.setRootIsDecorated( True );
+
   # get and return the key associated with the element we just clicked
   def slotFolderSelected(self,i):
     try:
       result = i.getKey()
       if not result is None:
+#       print result
         self.emit(PYSIGNAL("selected_vells_id"),(result,))
     except:
       pass
@@ -44,21 +71,21 @@ def main(args):
   app = QApplication(args)
 
   m_treeView =  VellsView()
-  m_treeView.addColumn("VellsTree")
-  m_treeView.setRootIsDecorated( True );
-  root = VellsElement( m_treeView, "root" )
+  root = VellsElement( m_treeView, "result" )
   a = VellsElement( root );
-  a.setText(0,"A")
-  a.setKey("Duck")
+  a.setText(0,"beginning")
   b = VellsElement( root, a)
-  b.setText(0,"B")
+  b.setText(0,"item 0")
   b.setKey("Pig")
-  c = VellsElement( root, b)
-  c.setText(0,"C")
+  e = VellsElement(root, b);
+  e.setText(0,"item 5")
+  e.setKey("Chicken")
+  c = VellsElement( root, e)
+  c.setText(0,"item 10")
   c.setKey("Ape")
-  temp = VellsElement(a);
-  temp.setText(0,"foo")
-  temp.setKey("Chicken")
+  d = VellsElement(a);
+  d.setText(0,"item one")
+  d.setKey("Donkey")
 
   app.setMainWidget( m_treeView)
   m_treeView.show()
