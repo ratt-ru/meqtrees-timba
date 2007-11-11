6edde28d5988e292b351469e33a6fbec3e7371f9
Tony Willis <twillis@drao.nrc.ca>
2007-11-11 19:28:31 +0000

    Script to form up phased array beam from AzEl telescope and rotate into sky reference frame

---
+++ WH/contrib/AGW/MG_AGW_sky_derotate.py
@@ -0,0 +1,352 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2002-2007
+# ASTRON (Netherlands Foundation for Research in Astronomy)
+# and The MeqTree Foundation
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands, seg@astron.nl
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+# Short description:
+# We read in a group of focal plane array beams, 
+# and form a combined, weighted beam with phase-conjugate weighting.
+# The weights are found by getting complex values at specified
+# L,M location in the X (|| poln) beams and then
+# taking the conjugate transpose.
+
+# We then pretend that we are observing with an AzEl mounted telescope
+# at the VLA site and calculate the change in phased-up beam
+# shape as we track a specific off-boresight RA/DEC point over
+# an 8 hr 'VLA' observation. The constant L,M (RA/DEC) position
+# has to be translated into the L',M' frame of the Az/El mount.
+
+# We then re-rotate the phased-up beam so that
+# we see what the beam looks like in the frame of the sky.
+
+# History:
+# - 07 Mar 2007: creation:
+
+#=======================================================================
+# Import of Python / TDL modules:
+
+# Get TDL and Meq for the Kernel
+from Timba.TDL import * 
+from Timba.Meq import meqds
+from Timba.Meq import meq
+from Meow import Bookmarks,Utils
+
+import os
+
+Settings.forest_state = record(bookmarks=[
+  record(name='I rot',page=Bookmarks.PlotPage(
+#     ["resampler_I"], ["IQUV_rot"],["IQUV"],
+      ["IQUV_rot"],
+  )),
+])
+
+# to force caching put 100
+Settings.forest_state.cache_policy = 100
+
+# get position of phase up point in L and M
+TDLCompileMenu('L and M position of phased-up beam',
+  TDLOption('l_beam','L position of beam (in units of FWHM)',[0,1,2,3],more=float),
+  TDLOption('m_beam','M position of beam (in units of FWHM)',[0,1,2,3],more=float),
+);
+
+# get directory with GRASP focal plane array beams
+TDLCompileOption('array_directory','directory with focal plane array files',['gauss_array_pats','gauss_array_pats_defocus','veidt_fpa_180', 'veidt_fpa_30'],more=str)
+
+########################################################
+def _define_forest(ns):  
+
+  # station position for 'pseudo' VLA telescope for 
+  # aips++ MVDirection object (ITRF units are metres)
+  X_pos = -1597262.96
+  Y_pos = -5043205.54
+  Z_pos = 3554901.34
+  ns.x_pos << Meq.Parm(X_pos,node_groups='Parm')
+  ns.y_pos << Meq.Parm(Y_pos,node_groups='Parm')
+  ns.z_pos << Meq.Parm(Z_pos,node_groups='Parm')
+  #create a  MeqComposer containing X_pos, Y_pos, Z_pos children
+  ns.XYZ <<Meq.Composer(ns.x_pos, ns.y_pos, ns.z_pos)
+
+  # now define a field centre RA and DEC (in radians) which the boresight
+  # will track - we arbitrarily choose Ra = 0, Dec = 28.6 deg (0.5 rad)
+  ns.ra0 << Meq.Constant(0.0)
+  ns.dec0 << Meq.Constant(0.5)
+# then create a MeqComposer containing the field centre RA and DEC as children
+  ns.RADec0 <<Meq.Composer(ns.ra0, ns.dec0)
+ 
+  # we should now be able to create an ParAngle node with X,Y,Z station positions
+  pa = ns.ParAngle << Meq.ParAngle(radec=ns.RADec0, xyz=ns.XYZ)
+
+  # define desired half-intensity width of power pattern (HPBW)
+  # as we are fitting total intensity I pattern (here .021 rad = 74.8 arcmin)
+  ns.fwhm << Meq.Constant(0.021747) # beam FWHM
+
+  # values for l_beam and m_beam are obtained from the TDLCompileMenu
+  ns.l_beam_c << ns.fwhm * l_beam
+  ns.m_beam_c << ns.fwhm * m_beam
+
+  ns.ll_mm_beam << Meq.Composer(ns.l_beam_c,ns.m_beam_c);
+  # we should now be able to create an LMRaDec node with the field
+  # centre RA and DEC and the L and M offsets as children.
+  # This node gives as output the RA and DEC corresponding to the
+  # specified L,M offset
+  ns.RaDec_beam << Meq.LMRaDec(radec_0=ns.RADec0, lm=ns.ll_mm_beam)
+
+# compute corresponding 'apparent' L,M position of feed in AzEl
+# system as function of parallactic angle
+  ns.lm_prime_beam << Meq.LMN(ns.RADec0, ns.RaDec_beam, ns.ParAngle)
+  ns.l_prime_beam << Meq.Selector(ns.lm_prime_beam, index=0)
+  ns.m_prime_beam << Meq.Selector(ns.lm_prime_beam, index=1)
+  ns.l_m_beam << Meq.Composer(ns.l_prime_beam,ns.m_prime_beam);
+
+# freeze beam position over 'tile' interval by getting the mean over interval
+  ns.lm_beam << Meq.Mean(ns.l_m_beam)
+
+  # number of beams is 30 or 90
+  if array_directory.find('30') >= 0:
+    num_beams = 30
+  else:
+    num_beams = 90
+
+  # read in beam images
+  BEAMS = range(0,num_beams)
+  for k in BEAMS:
+  # read in beam data - y dipole
+
+    infile_name_re_yx = array_directory + '/fpa_pat_' + str(k+num_beams) + '_Re_x.fits'
+    infile_name_im_yx = array_directory + '/fpa_pat_' + str(k+num_beams) +'_Im_x.fits'
+    infile_name_re_yy = array_directory + '/fpa_pat_' + str(k+num_beams) +'_Re_y.fits'
+    infile_name_im_yy = array_directory + '/fpa_pat_' + str(k+num_beams) +'_Im_y.fits' 
+    ns.image_re_yx(k) << Meq.FITSImage(filename=infile_name_re_yx,cutoff=1.0,mode=2)
+    ns.image_im_yx(k) << Meq.FITSImage(filename=infile_name_im_yx,cutoff=1.0,mode=2)
+    ns.image_re_yy(k) << Meq.FITSImage(filename=infile_name_re_yy,cutoff=1.0,mode=2)
+    ns.image_im_yy(k) << Meq.FITSImage(filename=infile_name_im_yy,cutoff=1.0,mode=2)
+    # normalize
+    ns.y_im_sq(k) << ns.image_re_yy(k) * ns.image_re_yy(k) + ns.image_im_yy(k) * ns.image_im_yy(k) +\
+                  ns.image_re_yx(k) * ns.image_re_yx(k) + ns.image_im_yx(k) * ns.image_im_yx(k)
+    ns.y_im(k) <<Meq.Sqrt(ns.y_im_sq(k))
+    ns.y_im_max(k) <<Meq.Max(ns.y_im(k))
+    ns.norm_image_re_yy(k) << ns.image_re_yy(k) / ns.y_im_max(k)
+    ns.norm_image_im_yy(k) << ns.image_im_yy(k) / ns.y_im_max(k)
+    ns.norm_image_re_yx(k) << ns.image_re_yx(k) / ns.y_im_max(k)
+    ns.norm_image_im_yx(k) << ns.image_im_yx(k) / ns.y_im_max(k)
+
+    ns.resampler_image_re_yy(k) << Meq.Resampler(ns.norm_image_re_yy(k),dep_mask = 0xff)
+    ns.resampler_image_im_yy(k) << Meq.Resampler(ns.norm_image_im_yy(k),dep_mask = 0xff)
+    ns.beam_wt_re_y(k) << Meq.Compounder(children=[ns.lm_beam,ns.resampler_image_re_yy(k)],common_axes=[hiid('l'),hiid('m')])
+    ns.beam_wt_im_y(k) << Meq.Compounder(children=[ns.lm_beam,ns.resampler_image_im_yy(k)],common_axes=[hiid('l'),hiid('m')])
+
+    ns.beam_wt_y(k) << Meq.ToComplex(ns.beam_wt_re_y(k), ns.beam_wt_im_y(k))
+    ns.beam_weight_y(k) << Meq.ConjTranspose(ns.beam_wt_y(k))
+
+    ns.beam_yx(k) << Meq.ToComplex(ns.norm_image_re_yx(k), ns.norm_image_im_yx(k)) 
+    ns.beam_yy(k) << Meq.ToComplex(ns.norm_image_re_yy(k), ns.norm_image_im_yy(k))
+    ns.wt_beam_yx(k) << ns.beam_yx(k) * ns.beam_weight_y(k)
+    ns.wt_beam_yy(k) << ns.beam_yy(k) * ns.beam_weight_y(k)
+
+  # read in beam data - x dipole
+    infile_name_re_xx = array_directory + '/fpa_pat_' + str(k) + '_Re_x.fits'
+    infile_name_im_xx = array_directory + '/fpa_pat_' + str(k) +'_Im_x.fits'
+    infile_name_re_xy = array_directory + '/fpa_pat_' + str(k) +'_Re_y.fits'
+    infile_name_im_xy = array_directory + '/fpa_pat_' + str(k) +'_Im_y.fits' 
+    ns.image_re_xy(k) << Meq.FITSImage(filename=infile_name_re_xy,cutoff=1.0,mode=2)
+    ns.image_im_xy(k) << Meq.FITSImage(filename=infile_name_im_xy,cutoff=1.0,mode=2)
+    ns.image_re_xx(k) << Meq.FITSImage(filename=infile_name_re_xx,cutoff=1.0,mode=2)
+    ns.image_im_xx(k) << Meq.FITSImage(filename=infile_name_im_xx,cutoff=1.0,mode=2)
+
+    # normalize
+    ns.x_im_sq(k) << ns.image_re_xx(k) * ns.image_re_xx(k) + ns.image_im_xx(k) * ns.image_im_xx(k) +\
+                  ns.image_re_xy(k) * ns.image_re_xy(k) + ns.image_im_xy(k) * ns.image_im_xy(k)
+    ns.x_im(k) <<Meq.Sqrt(ns.x_im_sq(k))
+    ns.x_im_max(k) <<Meq.Max(ns.x_im(k))
+    ns.norm_image_re_xx(k) << ns.image_re_xx(k) / ns.x_im_max(k)
+    ns.norm_image_im_xx(k) << ns.image_im_xx(k) / ns.x_im_max(k)
+    ns.norm_image_re_xy(k) << ns.image_re_xy(k) / ns.x_im_max(k)
+    ns.norm_image_im_xy(k) << ns.image_im_xy(k) / ns.x_im_max(k)
+
+
+    ns.resampler_image_re_xx(k) << Meq.Resampler(ns.norm_image_re_xx(k),dep_mask = 0xff)
+    ns.resampler_image_im_xx(k) << Meq.Resampler(ns.norm_image_im_xx(k),dep_mask = 0xff)
+    ns.beam_wt_re_x(k) << Meq.Compounder(children=[ns.lm_beam,ns.resampler_image_re_xx(k)],common_axes=[hiid('l'),hiid('m')])
+    ns.beam_wt_im_x(k) << Meq.Compounder(children=[ns.lm_beam,ns.resampler_image_im_xx(k)],common_axes=[hiid('l'),hiid('m')])
+
+    ns.beam_wt_x(k) << Meq.ToComplex(ns.beam_wt_re_x(k), ns.beam_wt_im_x(k))
+    ns.beam_weight_x(k) << Meq.ConjTranspose(ns.beam_wt_x(k))
+
+    ns.beam_xy(k) << Meq.ToComplex(ns.norm_image_re_xy(k), ns.norm_image_im_xy(k)) 
+    ns.beam_xx(k) << Meq.ToComplex(ns.norm_image_re_xx(k), ns.norm_image_im_xx(k))
+    ns.wt_beam_xy(k) << ns.beam_xy(k) * ns.beam_weight_x(k)
+    ns.wt_beam_xx(k) << ns.beam_xx(k) * ns.beam_weight_x(k)
+
+  ns.voltage_sum_xx << Meq.Add(*[ns.wt_beam_xx(k) for k in BEAMS])
+  ns.voltage_sum_xy << Meq.Add(*[ns.wt_beam_xy(k) for k in BEAMS])
+  ns.voltage_sum_yx << Meq.Add(*[ns.wt_beam_yx(k) for k in BEAMS])
+  ns.voltage_sum_yy << Meq.Add(*[ns.wt_beam_yy(k) for k in BEAMS])
+
+  # normalize beam to peak response
+  ns.voltage_sum_xx_r << Meq.Real(ns.voltage_sum_xx)
+  ns.voltage_sum_xx_i << Meq.Imag(ns.voltage_sum_xx)
+  ns.voltage_sum_xy_r << Meq.Real(ns.voltage_sum_xy)
+  ns.voltage_sum_xy_i << Meq.Imag(ns.voltage_sum_xy)
+  ns.im_sq_x << ns.voltage_sum_xx_r * ns.voltage_sum_xx_r + ns.voltage_sum_xx_i * ns.voltage_sum_xx_i +\
+                  ns.voltage_sum_xy_r * ns.voltage_sum_xy_r + ns.voltage_sum_xy_i * ns.voltage_sum_xy_i
+  ns.im_x <<Meq.Sqrt(ns.im_sq_x)
+  ns.im_x_max <<Meq.Max(ns.im_x)
+  ns.voltage_sum_xx_norm << ns.voltage_sum_xx / ns.im_x_max
+  ns.voltage_sum_xy_norm << ns.voltage_sum_xy / ns.im_x_max
+
+  ns.voltage_sum_yy_r << Meq.Real(ns.voltage_sum_yy)
+  ns.voltage_sum_yy_i << Meq.Imag(ns.voltage_sum_yy)
+  ns.voltage_sum_yx_r << Meq.Real(ns.voltage_sum_yx)
+  ns.voltage_sum_yx_i << Meq.Imag(ns.voltage_sum_yx)
+  ns.im_sq_y << ns.voltage_sum_yy_r * ns.voltage_sum_yy_r + ns.voltage_sum_yy_i * ns.voltage_sum_yy_i +\
+                  ns.voltage_sum_yx_r * ns.voltage_sum_yx_r + ns.voltage_sum_yx_i * ns.voltage_sum_yx_i
+  ns.im_y <<Meq.Sqrt(ns.im_sq_y)
+  ns.im_y_max <<Meq.Max(ns.im_y)
+  ns.voltage_sum_yy_norm << ns.voltage_sum_yy / ns.im_y_max
+  ns.voltage_sum_yx_norm << ns.voltage_sum_yx / ns.im_y_max
+
+  ns.E << Meq.Matrix22(ns.voltage_sum_xx_norm, ns.voltage_sum_yx_norm,ns.voltage_sum_xy_norm, ns.voltage_sum_yy_norm)
+  ns.Et << Meq.ConjTranspose(ns.E)
+
+ # sky brightness
+  ns.B0 << 0.5 * Meq.Matrix22(1.0, 0.0, 0.0, 1.0)
+
+  # observe!
+  ns.observed << Meq.MatrixMultiply(ns.E, ns.B0, ns.Et)
+
+  # extract I,Q,U,V etc
+  ns.IpQ << Meq.Selector(ns.observed,index=0)        # XX = (I+Q)/2
+  ns.ImQ << Meq.Selector(ns.observed,index=3)        # YY = (I-Q)/2
+  ns.I << Meq.Add(ns.IpQ,ns.ImQ)                     # I = XX + YY
+  ns.Q << Meq.Subtract(ns.IpQ,ns.ImQ)                # Q = XX - YY
+
+  ns.UpV << Meq.Selector(ns.observed,index=1)        # XY = (U+iV)/2
+  ns.UmV << Meq.Selector(ns.observed,index=2)        # YX = (U-iV)/2
+  ns.U << Meq.Add(ns.UpV,ns.UmV)                     # U = XY + YX
+  ns.iV << Meq.Subtract(ns.UpV,ns.UmV)               # iV = XY - YX  <----!!
+  ns.V << ns.iV / 1j                                 # V = iV / i
+                                                     # (note: i => j in Python)
+
+  # attempt to de-rotate the I image
+  ns.l << Meq.Grid(axis=2);     # returns l(l) = l
+  ns.m << Meq.Grid(axis=3);     # returns m(m) = m
+  ns.lm_pre_rot << Meq.Composer(ns.l,ns.m)    # returns an lm 2-vector
+
+  ns.P << Meq.Matrix22(Meq.Cos(pa),Meq.Sin(pa),-Meq.Sin(pa),Meq.Cos(pa)) 
+  ns.rot_lm << Meq.MatrixMultiply(ns.P,ns.lm_pre_rot);    # rotated lm
+  ns.l_rot << Meq.Selector(ns.rot_lm,index=0)
+  ns.m_rot << Meq.Selector(ns.rot_lm,index=1)
+  ns.lm_rot << Meq.Composer(Meq.Grid(axis=0),Meq.Grid(axis=1),ns.l_rot,ns.m_rot)
+
+  # the resampler seems to only work on 1 VellSet so we have to
+  # do each polarization separately.
+  ns.I_real << Meq.Real(ns.I)
+  ns.resampler_I << Meq.Resampler(ns.I_real,dep_mask = 0xff)
+  ns.I_rot << Meq.Compounder(children=[ns.lm_rot,ns.resampler_I],common_axes=[hiid('l'),hiid('m')])
+  ns.Q_real << Meq.Real(ns.Q)
+  ns.resampler_Q << Meq.Resampler(ns.Q_real,dep_mask = 0xff)
+  ns.Q_rot << Meq.Compounder(children=[ns.lm_rot,ns.resampler_Q],common_axes=[hiid('l'),hiid('m')])
+  ns.U_real << Meq.Real(ns.U)
+  ns.resampler_U << Meq.Resampler(ns.U_real,dep_mask = 0xff)
+  ns.U_rot << Meq.Compounder(children=[ns.lm_rot,ns.resampler_U],common_axes=[hiid('l'),hiid('m')])
+  ns.V_real << Meq.Real(ns.V)
+  ns.resampler_V << Meq.Resampler(ns.V_real,dep_mask = 0xff)
+  ns.V_rot << Meq.Compounder(children=[ns.lm_rot,ns.resampler_V],common_axes=[hiid('l'),hiid('m')])
+
+  # join together into one node in order to make a single request
+  ns.IQUV_rot << Meq.Composer(ns.I_rot, ns.Q_rot,ns.U_rot, ns.V_rot)
+
+########################################################################
+def _test_forest(mqs,parent):
+
+# any large time range will do: we observe the changes in the beam
+# pattern in timesteps of 3600s, or 1 hr
+  delta_t = 3600.0
+  # t0 = -1200.0 + 0.5 * delta_t
+  # Approx start of 2001
+  t0 = 4485011731.14 - delta_t       
+  t1 = t0 + delta_t
+
+  f0 = 800.0
+  f1 = 1300.0
+
+  m_range = [-0.15,0.15];
+  l_range = [-0.15,0.15];
+  lm_num = 101;
+  counter = 0
+  for i in range(8):
+      t0 = t0 + delta_t
+      t1 = t0 + delta_t
+      mqs.clearcache('IQUV_rot',recursive=True)
+      request = make_request(counter=counter, dom_range = [[f0,f1],[t0,t1],l_range,m_range], nr_cells = [1,1,lm_num,lm_num])
+      counter = counter + 1
+# execute request
+      mqs.meq('Node.Execute',record(name='IQUV_rot',request=request),wait=False);
+
+#####################################################################
+def make_request(counter=0,Ndim=4,dom_range=[0.,1.],nr_cells=5):
+
+    """make multidimensional request, dom_range should have length 2 or be a list of
+    ranges with length Ndim, nr_cells should be scalar or list of scalars with length Ndim"""
+    forest_state=meqds.get_forest_state();
+    axis_map=forest_state.axis_map;
+    
+    range0 = [];
+    if is_scalar(dom_range[0]):
+        for i in range(Ndim):		
+            range0.append(dom_range);
+    else:
+        range0=dom_range;
+    nr_c=[];
+    if is_scalar(nr_cells):
+        for i in range(Ndim):		
+            nr_c.append(nr_cells);
+    else:
+        nr_c =nr_cells;
+    dom = meq.domain(range0[0][0],range0[0][1],range0[1][0],range0[1][1]); #f0,f1,t0,t1
+    cells = meq.cells(dom,num_freq=nr_c[0],num_time=nr_c[1]);
+    
+    # workaround to get domain with more axes running 
+
+    for dim in range(2,Ndim):
+        id = axis_map[dim].id;
+        if id:
+            dom[id] = [float(range0[dim][0]),float(range0[dim][1])];
+            step_size=float(range0[dim][1]-range0[dim][0])/nr_c[dim];
+            startgrid=0.5*step_size+range0[dim][0];
+            grid = [];
+            cell_size=[];
+        for i in range(nr_c[dim]):
+            grid.append(i*step_size+startgrid);
+            cell_size.append(step_size);
+            cells.cell_size[id]=array(cell_size);
+            cells.grid[id]=array(grid);
+            cells.segments[id]=record(start_index=0,end_index=nr_c[dim]-1);
+
+    cells.domain=dom;
+    rqid = meq.requestid(domain_id=counter)
+    request = meq.request(cells,rqtype='ev',rqid=rqid);
+    return request;
+
+if __name__=='__main__':
+  ns=NodeScope()
+  _define_forest(ns)
+  ns.Resolve()
+  print "Added %d nodes" % len(ns.AllNodes())
