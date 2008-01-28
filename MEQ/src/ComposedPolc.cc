0df70c419288fa74b5f4cce05bf0580480c5f2ff
Maaijke Mevius <mevius@astron.nl>
2008-01-28 12:19:05 +0000

    added possibility to evaluate funklets on non_defined axis

--- MEQ/src/ComposedPolc.cc
+++ MEQ/src/ComposedPolc.cc
@@ -271,6 +271,9 @@ void ComposedPolc::validateContent (bool recursive)
 	if (!cell_dom.isDefined(axisi)){
 	  starti[axisi]=0;endi[axisi]=0;continue;
 	}
+	if (!polcdom.isDefined(axisi)){
+	  starti[axisi]=0;endi[axisi]=std::min(res_shape[axisi]-1,startgrid[axisi].size()-1);continue;
+	}
 	int maxk=std::min(res_shape[axisi],startgrid[axisi].size());
 	int k=0;
 	while(k<maxk  && centergrid[axisi](k)<polcdom.start(axisi)) k++;
