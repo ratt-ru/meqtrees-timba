c9545d0828a60eb4e8059dbab64ed929910d329a
Oleg Smirnov <osmirnov@gmail.com>
2008-02-20 18:38:02 +0000

    Sources in a Meow.Patch are now added in hierarchical sets of two. This should lead to a smaller memory footprint.

--- WH/contrib/OMS/Meow/Patch.py
+++ WH/contrib/OMS/Meow/Patch.py
@@ -43,7 +43,27 @@ class Patch (SkyComponent):
     else:
       compvis = [ comp.visibilities(array,observation) for comp in self._components ];
       # add them up per-ifr
-      [ nodes(*ifr) << Meq.Add(*[vis(*ifr) for vis in compvis])
-        for ifr in array.ifrs()
-      ];
+      # be clever -- having too many children to one node leads to a huge
+      # memory footprint, so we add them in packs of two. cur_vis is the current
+      # list of nodes to be added, it is reduced by a factor of 2 at each step.
+      sums = compvis;
+      while sums:
+        # if down to two terms or less, generate final nodes
+        if len(sums) <= 2:
+          for ifr in array.ifrs():
+            nodes(*ifr) << Meq.Add(*[x(*ifr) for x in sums])
+          break;
+        # else generate intermediate sums
+        else:
+          newsums = [];
+          for i in range(0,len(sums),2):
+            # if we're dealing with the odd term out, propagate it to newsums list as-is
+            if i == len(sums)-1:
+              newsums.append(sums[i]);
+            else:
+              newnode = nodes('(%d,%d)'%(len(sums),i));  # create unique name for intermediate sum node
+              for ifr in array.ifrs():
+                newnode(*ifr) << Meq.Add(*[x(*ifr) for x in sums[i:i+2]]);
+              newsums.append(newnode);
+          sums = newsums;
     return nodes;
