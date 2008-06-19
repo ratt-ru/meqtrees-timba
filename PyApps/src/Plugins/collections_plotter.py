98f56ca2484989ecb502307e4132bcbdb53b5f19
Tony Willis <twillis@drao.nrc.ca>
2008-06-19 18:14:59 +0000

    Commented out option to make this display the 'default' for a Composer node

--- PyApps/src/Plugins/collections_plotter.py
+++ PyApps/src/Plugins/collections_plotter.py
@@ -282,6 +282,6 @@ class CollectionsPlotter(GriddedPlugin):
     self.flash_refresh();
     _dprint(3, 'exiting process_data')
 
-Grid.Services.registerViewer(meqds.NodeClass('MeqComposer'),CollectionsPlotter,priority=10)
+#Grid.Services.registerViewer(meqds.NodeClass('MeqComposer'),CollectionsPlotter,priority=10)
 Grid.Services.registerViewer(meqds.NodeClass(),CollectionsPlotter,priority=22)
 
