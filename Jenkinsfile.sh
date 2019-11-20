e3edd8e103f5621a86e2464f2f2a428c3a45d944
Benjamin Hugo <bennahugo@gmail.com>
2019-11-20 11:39:13 +0200

    Add python 3 test (#52)

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -21,4 +21,5 @@ cd $PROJECTS_DIR/meqtrees-timba
 IMAGENAME="mttimbapr"
 
 # build and test
-docker build -f .travis/py2.docker -t "$IMAGENAME:$BUILD_NUMBER" --no-cache=true .
+docker build -f .travis/py2.docker -t "${IMAGENAME}27:$BUILD_NUMBER" --no-cache=true .
+docker build -f .travis/py3.docker -t "${IMAGENAME}36:$BUILD_NUMBER" --no-cache=true .
