a7a81a57938bf78491d6056cbfccf05d993f8a7d
bennahugo <bennahugo@gmail.com>
2022-01-13 18:17:58 +0200

    remove references to purr in meqbrowser.py frontend in symlinktree

--- install/make-symlink-tree.sh
+++ install/make-symlink-tree.sh
@@ -96,8 +96,8 @@ cd python
 #ln-s ../../../../FW/Siamese
 #ln-s ../../../../FW/Calico
 #ln-s ../../../../FW/Ionosphere
-ln-s ../../../../PyApps/src/Purr
-ln-s ../../../../PyApps/src/meqbrowser.py
+#ln-s ../../../../PyApps/src/Purr
+#ln-s ../../../../PyApps/src/meqbrowser.py
 makedir Timba
 cd Timba
 ln-s ../../../../../PyApps/src/Apps
